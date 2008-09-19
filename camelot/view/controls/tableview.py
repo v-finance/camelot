ac1482de584ff6135e2d9f1701f792ffe3015c98
victorn <victorn@8325946a-aa55-0410-8760-f0bc8c33efaa>
2008-09-19 13:46:58 +0000

    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@43 8325946a-aa55-0410-8760-f0bc8c33efaa

--- camelot/view/controls/tableview.py
+++ camelot/view/controls/tableview.py
@@ -3,17 +3,22 @@ import settings
 logger = logging.getLogger('view.controls.tableview')
 
 from PyQt4 import QtCore, QtGui
-from PyQt4.QtCore import Qt
+from QtGui import QSizePolicy
+from PyQt4.QtCore import Qt, SIGNAL
 from camelot.view.proxy.queryproxy import QueryTableProxy
 
+
 class QueryTable(QtGui.QTableView):
+
   def __init__(self):
     QtGui.QTableView.__init__(self)
-    logger.debug('create querytable')      
+    logger.debug('create querytable')
     self.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
-    self.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
-                           
+    self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+
 class TableView(QtGui.QWidget):
+
   def __init__(self, admin, parent):
     from search import SimpleSearchControl
     from inheritance import SubclassTree
@@ -33,19 +38,14 @@ class TableView(QtGui.QWidget):
     self.table_layout.insertWidget(0, self.search_control)
     self.setSubclass(admin)
     self.class_tree = SubclassTree(admin, self)
-    self.widget_layout.insertWidget(0, self.class_tree )
+    self.widget_layout.insertWidget(0, self.class_tree)
     self.widget_layout.insertLayout(1, self.table_layout)
-    self.connect(self.search_control, QtCore.SIGNAL('search'), self.startSearch)
-    self.connect(self.search_control, QtCore.SIGNAL('cancel'), self.cancelSearch)
-    self.connect(self.class_tree, QtCore.SIGNAL('subclasssClicked'), self.setSubclass)
-    self.search_filter = lambda q:q
+    self.connect(self.search_control, SIGNAL('search'), self.startSearch)
+    self.connect(self.search_control, SIGNAL('cancel'), self.cancelSearch)
+    self.connect(self.class_tree, SIGNAL('subclasssClicked'), self.setSubclass)
+    self.search_filter = lambda q: q
     self.setLayout(self.widget_layout)
     self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
-#        # should occupy 1/4 of parent space
-#        if parent:
-#          width = int(parent.width() / 2)
-#          height = int(parent.height() / 2)
-#          self.resize(width, height)
 
   def setSubclass(self, admin):
     """Switch to a different subclass, where admin is the admin object of the
@@ -54,41 +54,49 @@ class TableView(QtGui.QWidget):
     if self.table:
       self.table.deleteLater()
       self.table_model.deleteLater()
-    self.table = QueryTable()         
-    # We create the table first with only 10 rows, to be able resize the columns to
-    # the contents without much processing
-    self.table_model = QueryTableProxy(admin, admin.entity.query.limit(10), admin.getColumns)
+    self.table = QueryTable()
+    # We create the table first with only 10 rows, to be able resize
+    # the columns to the contents without much processing
+    self.table_model = QueryTableProxy(admin,
+                                       admin.entity.query.limit(10),
+                                       admin.getColumns)
     self.table.setModel(self.table_model)
     self.table_layout.insertWidget(1, self.table)
-    
+
     def update_delegates(*args):
       self.table.setItemDelegate(self.table_model.getItemDelegate())
-      
-    admin.mt.post(lambda:None, update_delegates)
+
+    admin.mt.post(lambda: None, update_delegates)
     # Once those are loaded, rebuild the query to get the actual number of rows
-    admin.mt.post(lambda:self.table_model._extend_cache(0, 10), lambda x:self.resizeColumnsAndRebuildQuery())
-    admin.mt.post(lambda:admin.getFilters(), lambda items:self.setFilters(items))
-    admin.mt.post(lambda:admin.getListCharts(), lambda charts:self.setCharts(charts))
-       
+    admin.mt.post(lambda: self.table_model._extend_cache(0, 10),
+                  lambda x: self.resizeColumnsAndRebuildQuery())
+    admin.mt.post(lambda: admin.getFilters(),
+                  lambda items: self.setFilters(items))
+    admin.mt.post(lambda: admin.getListCharts(),
+                  lambda charts: self.setCharts(charts))
+
   def setCharts(self, charts):
     if charts:
-      
-      from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+
       from matplotlib.figure import Figure
-      
+      from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as \
+                                                     FigureCanvas
+
       chart = charts[0]
-      
+
       def getData():
         from sqlalchemy.sql import select, func
         from elixir import session
         xcol = getattr(self.admin.entity, chart['x'])
         ycol = getattr(self.admin.entity, chart['y'])
         session.bind = self.admin.entity.table.metadata.bind
-        summary = session.execute(select([xcol, func.sum(ycol)]).group_by(xcol)).fetchall()
-        return [s[0] for s in summary],[s[1] for s in summary]
-  
+        result = session.execute(select([xcol, func.sum(ycol)]).group_by(xcol))
+        summary = result.fetchall()
+        return [s[0] for s in summary], [s[1] for s in summary]
+
       class MyMplCanvas(FigureCanvas):
-          """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
+          """Ultimately, this is a QWidget (as well as a FigureCanvasAgg)"""
+
           def __init__(self, parent=None, width=5, height=4, dpi=100):
               fig = Figure(figsize=(width, height), dpi=dpi, facecolor='w')
               self.axes = fig.add_subplot(111, axisbg='w')
@@ -98,32 +106,33 @@ class TableView(QtGui.QWidget):
               FigureCanvas.__init__(self, fig)
               self.setParent(parent)
               FigureCanvas.setSizePolicy(self,
-                                         QtGui.QSizePolicy.Expanding,
-                                         QtGui.QSizePolicy.Expanding)
+                                         QSizePolicy.Expanding,
+                                         QSizePolicy.Expanding)
               FigureCanvas.updateGeometry(self)
-      
+
           def compute_initial_figure(self):
               pass
-      
+
       def setData(data):
-        
+
         class MyStaticMplCanvas(MyMplCanvas):
             """Simple canvas with a sine plot."""
+
             def compute_initial_figure(self):
                 x, y = data
-                bar_positions = [i-0.25 for i in range(1,len(x)+1)]
+                bar_positions = [i-0.25 for i in range(1, len(x)+1)]
                 width = 0.5
                 self.axes.bar(bar_positions, y, width, color='b')
                 self.axes.set_xlabel('Year')
                 self.axes.set_ylabel('Sales')
                 self.axes.set_xticks(range(len(x)+1))
                 self.axes.set_xticklabels(['']+[str(d) for d in x])
-              
+
         sc = MyStaticMplCanvas(self, width=5, height=4, dpi=100)
-        self.table_layout.addWidget( sc )
-        
+        self.table_layout.addWidget(sc)
+
       self.admin.mt.post(getData, setData)
-  
+
   def resizeColumnsAndRebuildQuery(self):
     logger.debug('resizeColumnsAndRebuildQuery')
     self.table.resizeColumnsToContents()
@@ -131,43 +140,43 @@ class TableView(QtGui.QWidget):
 
     logger.debug('Selecting first row in table')
     self.table.selectRow(0)
-    
+
   def deleteSelectedRows(self):
     """Delete the selected rows in this tableview"""
     logger.debug('delete selected rows called')
     for row in set(map(lambda x: x.row(), self.table.selectedIndexes())):
       self.table_model.removeRow(row, None)
-      
+
   def newRow(self):
     """Create a new row in the tableview"""
     self.table_model.insertRow(0, None)
-            
+
   def selectTableRow(self, row):
     self.table.selectRow(row)
 
   def selectedTableIndexes(self):
     return self.table.selectedIndexes()
-  
+
   def getColumns(self):
     return self.admin.getColumns()
-    
+
   def getData(self):
     for d in self.table_model.getData():
       yield d
-    
+
   def getTitle(self):
     return self.admin.getName()
-    
+
   def rebuildQuery(self):
     query = self.admin.entity.query
     if self.filters:
       query = self.filters.decorate_query(query)
-    self.table_model.setQuery( self.search_filter(query) )
-    
+    self.table_model.setQuery(self.search_filter(query))
+
   def startSearch(self, text):
     logger.debug('search %s' % text)
     import camelot.types
-      
+
     def create_search_filter():
       if len(text.strip()):
         from sqlalchemy import Unicode, or_
@@ -175,25 +184,27 @@ class TableView(QtGui.QWidget):
         for c in self.admin.entity.table._columns:
             if issubclass(c.type.__class__, camelot.types.Code):
               codes = text.split('.')
-              args.append( c.like(['%'] + codes + ['%']) )
-              args.append( c.like(['%'] + codes) )
-              args.append( c.like(codes + ['%']) )
-            elif issubclass(c.type.__class__, (Unicode,)) or (hasattr(c.type, 'impl') and issubclass(c.type.impl.__class__, (Unicode,))):
+              args.append(c.like(['%'] + codes + ['%']))
+              args.append(c.like(['%'] + codes))
+              args.append(c.like(codes + ['%']))
+            elif issubclass(c.type.__class__, (Unicode, )) or \
+                            (hasattr(c.type, 'impl') and \
+                             issubclass(c.type.impl.__class__, (Unicode, ))):
               logger.debug('look in column : %s'%c.name)
-              args.append( c.like('%'+text+'%') )
+              args.append(c.like('%'+text+'%'))
         if len(args):
             if len(args)>1:
-                return lambda q:q.filter(or_(*args))
+                return lambda q: q.filter(or_(*args))
             else:
-                return lambda q:q.filter(args[0])
-      return lambda q:q
+                return lambda q: q.filter(args[0])
+      return lambda q: q
 
     self.search_filter = create_search_filter()
     self.rebuildQuery()
 
   def cancelSearch(self):
     logger.debug('cancel search')
-    self.search_filter = lambda q:q
+    self.search_filter = lambda q: q
     self.rebuildQuery()
 
   def setFilters(self, items):
@@ -205,18 +216,18 @@ class TableView(QtGui.QWidget):
     if items:
       self.filters = FilterList(items, self)
       self.widget_layout.insertWidget(2, self.filters)
-      self.connect(self.filters, QtCore.SIGNAL('filters_changed'), self.rebuildQuery)
+      self.connect(self.filters, SIGNAL('filters_changed'), self.rebuildQuery)
 
   def toHtml(self):
-    table = [[getattr(row,col[0]) for col in self.admin.getColumns()] for row in self.admin.entity.query.all()]
+    table = [[getattr(row, col[0]) for col in self.admin.getColumns()]
+             for row in self.admin.entity.query.all()]
     context = {
       'title': self.admin.getName(),
       'table': table,
       'columns': [c[0] for c in self.admin.getColumns()],
     }
     from jinja import Environment, FileSystemLoader
-    e = Environment(loader=FileSystemLoader(settings.CAMELOT_TEMPLATES_DIRECTORY))
-    t = e.get_template('table_view.html')
-    return t.render(context)
-
-    
+    ld = FileSystemLoader(settings.CAMELOT_TEMPLATES_DIRECTORY)
+    env = Environment(loader=ld)
+    tp = env.get_template('table_view.html')
+    return tp.render(context)
