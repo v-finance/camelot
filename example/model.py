0c7f4b8e48d1afe3cc6c8a796fb35e2f85d92747
victorn <victorn@8325946a-aa55-0410-8760-f0bc8c33efaa>
2008-09-19 14:39:20 +0000

    *fixed trailing whitespaces
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@47 8325946a-aa55-0410-8760-f0bc8c33efaa

--- example/model.py
+++ example/model.py
@@ -1,18 +1,19 @@
-# 
+#
 # Example model file, populate this file with your own models
 # to get started quickly
 #
 
-from datetime               import datetime
+from datetime import datetime
 
 import camelot.types
 from camelot.model import *
+from camelot.view.elixir_admin import EntityAdmin
 
 __metadata__ = metadata
 
-from camelot.view.elixir_admin import EntityAdmin
 
 class Director(Entity):
+
     name = Field(Unicode(60))
     movies = OneToMany('Movie', inverse='director')
     using_options(tablename='directors')
@@ -20,7 +21,7 @@ class Director(Entity):
     def __repr__(self):
       return self.name
 
-    # 
+    #
     # Each Entity subclass can have a subclass of EntityAdmin as
     # its inner class.  The EntityAdmin class defines how the Entity
     # class will be displayed in the GUI.  Its behaviour can be steered
@@ -29,18 +30,20 @@ class Director(Entity):
     # To fully customize the way the entity is visualized, the EntityAdmin
     # subclass should overrule some of the EntityAdmin's methods
     #
+
     class Admin(EntityAdmin):
         name = 'Directors'
         # the section attributed specifies where in the left panel of the
-        # main window a link to a list of this entities will appear.  Have
+        # main window a link to a list of this entities will appear. Have
         # a look in main.py for the definition of the sections
         section = 'movies'
         # the list_display attribute specifies which entity attributes should
         # be visible in the table view
         list_display = ['name']
-        # the fields attribute specifies which entity attributes should be visible
-        # in the form view
+        # the fields attribute specifies which entity attributes should be
+        # visible in the form view
         fields = ['name', 'movies']
+
 
 class Movie(Entity):
     title = Field(Unicode(60))
@@ -49,15 +52,15 @@ class Movie(Entity):
     director = ManyToOne('Director', inverse='movies')
     actors = ManyToMany('Actor', inverse='movies', tablename='movie_casting')
     #
-    # Camelot includes custom sqlalchemy types, like Image, which stores an PIL image
-    # on disk and keeps the reference to it in the database.
+    # Camelot includes custom sqlalchemy types, like Image, which stores an
+    # PIL image on disk and keeps the reference to it in the database.
     #
     cover = Field(camelot.types.Image(upload_to='covers'))
     using_options(tablename='movies')
 
     def burn_to_disk(self):
       print 'burn burn burn'
-      
+
     class Admin(EntityAdmin):
         name = 'Movies'
         section = 'movies'
@@ -66,21 +69,22 @@ class Movie(Entity):
         #
         # create a list of actions available for the user on the form view
         # those actions will be executed within the model thread
-        # 
-        form_actions = [('Burn DVD',lambda o:o.burn_to_disk())]
+        #
+        form_actions = [('Burn DVD', lambda o: o.burn_to_disk())]
 
     def __repr__(self):
       return self.title
-  
+
+
 class Actor(Entity):
     name = Field(Unicode(60))
     movies = ManyToMany('Movie', inverse='actors', tablename='movie_casting')
     using_options(tablename='actors')
-   
+
     class Admin(EntityAdmin):
         name = 'Actor'
         section = 'movies'
-        list_display = ['name',]
+        list_display = ['name', ]
         fields = ['name', 'movies']
 
     def __repr__(self):
