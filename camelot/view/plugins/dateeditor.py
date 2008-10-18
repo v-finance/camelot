d7c6e9ba99bc4ebe41fa56df58d51727b7def67c
erikj <erikj@8325946a-aa55-0410-8760-f0bc8c33efaa>
2008-10-18 11:04:53 +0000

    - add plugin for designer
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@126 8325946a-aa55-0410-8760-f0bc8c33efaa

---
+++ camelot/view/plugins/dateeditor.py
@@ -0,0 +1,58 @@
+#  ============================================================================
+#
+#  Copyright (C) 2007-2008 Conceptive Engineering bvba. All rights reserved.
+#  www.conceptive.be / project-camelot@conceptive.be
+#
+#  This file is part of the Camelot Library.
+#
+#  This file may be used under the terms of the GNU General Public
+#  License version 2.0 as published by the Free Software Foundation
+#  and appearing in the file LICENSE.GPL included in the packaging of
+#  this file.  Please review the following information to ensure GNU
+#  General Public Licensing requirements will be met:
+#  http://www.trolltech.com/products/qt/opensource.html
+#
+#  If you are unsure which license is appropriate for your use, please
+#  review the following information:
+#  http://www.trolltech.com/products/qt/licensing.html or contact
+#  project-camelot@conceptive.be.
+#
+#  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+#  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  For use of this library in commercial applications, please contact
+#  project-camelot@conceptive.be
+#
+#  ============================================================================
+
+from PyQt4 import QtGui, QtDesigner
+
+class DateEditorPlugin(QtDesigner.QPyDesignerCustomWidgetPlugin):
+  def __init__(self, parent = None):
+    QtDesigner.QPyDesignerCustomWidgetPlugin.__init__(self)
+    self.initialized = False
+  def initialize(self, core):
+    if self.initialized:
+      return
+    self.initialized = True
+  def isInitialized(self):
+    return self.initialized
+  def createWidget(self, parent):
+    from camelot.view.controls.editors import DateEditor
+    return DateEditor(parent=parent)
+  def name(self):
+    return "DateEditor"
+  def group(self):
+    return "Camelot"
+#  def icon(self):
+#    return QtGui.QIcon(_logo_pixmap)
+  def toolTip(self):
+    return ""
+  def whatsThis(self):
+    return ""
+  def isContainer(self):
+    return False
+  def domXml(self):
+    return '<widget class="DateEditor" name=\"DateEditor\" />\n'
+  def includeFile(self):
+    return "camelot.view.controls.editors"
