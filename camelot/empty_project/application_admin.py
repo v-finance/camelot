a0e62b39d5ce05821b67a46bf332f528005870ea
victorn <victorn@8325946a-aa55-0410-8760-f0bc8c33efaa>
2009-01-28 22:27:05 +0000

    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@254 8325946a-aa55-0410-8760-f0bc8c33efaa

--- camelot/empty_project/application_admin.py
+++ camelot/empty_project/application_admin.py
@@ -1,12 +1,18 @@
 from camelot.view.application_admin import ApplicationAdmin
-from camelot.view import art
+from camelot.view.art import TangoIcon
 
 class MyApplicationAdmin(ApplicationAdmin):
   
   def __init__(self):
-    super(MyApplicationAdmin, self).__init__([('relations', ('Relations', art.icon24('apps/system-users'))),
-                                              ('configuration',('Configuration', art.icon24('categories/preferences-system'))),]
-                                              )
+    super(MyApplicationAdmin, self).__init__([
+      ('relations',
+       ('Relations',
+        TangoIcon('system-users', folder='apps', size='24x24').fullpath())),
+      ('configuration',
+        ('Configuration',
+         TangoIcon('preferences-system', folder='categories', size='24x24'))),
+    ])
+
     from camelot.model.memento import Memento
     from camelot.model.authentication import Person, Organization
     from camelot.model.i18n import Translation
