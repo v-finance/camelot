dad4f4efbefac6cb86fdd3591bb8742ad6c0380f
erikj <erikj@8325946a-aa55-0410-8760-f0bc8c33efaa>
2009-03-12 07:28:56 +0000

    - pre / post processors for word export
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@334 8325946a-aa55-0410-8760-f0bc8c33efaa

--- camelot/view/export/word.py
+++ camelot/view/export/word.py
@@ -1,10 +1,23 @@
 import logging
+from camelot.view import art
+
 logger = logging.getLogger('camelot.view.export.word')
 
-def open_html_in_word(html):
+def open_html_in_word(html, template=art.file_('empty_document.doc'),
+                      pre_processor=lambda doc:None,
+                      post_processor=lambda doc:None):
+  """Open MS Word through COM objects and import the specified html
+  into a new document.
+  @param html: the html to import
+  @param template: the empty word document in which to import the html
+  @param post_processor: a function that will be called before importing the
+  html, with as its argument the COM Document.     
+  @param post_processor: a function that will be called after importing the
+  html, with as its argument the COM Document.   
+  """
   import tempfile
   import os
-  from camelot.view import art
+  
   html_fd, html_fn = tempfile.mkstemp(suffix='.html')
   html_file = os.fdopen(html_fd, 'wb')
   html_file.write(html.encode('utf-8'))
@@ -24,7 +37,11 @@ def open_html_in_word(html):
   doc_fd, doc_fn = tempfile.mkstemp(suffix='.doc')
   os.close(doc_fd)
   word_app.Visible = True
-  doc = word_app.Documents.Open(art.file_('empty_document.doc'))
+  doc = word_app.Documents.Open(template)
   word_app.ActiveDocument.SaveAs(doc_fn)
   section = doc.Sections(1)
+  pre_processor(doc)
   section.Range.InsertFile(FileName=html_fn)
+  post_processor(doc)
+  doc.Activate()
+  word_app.Activate()
