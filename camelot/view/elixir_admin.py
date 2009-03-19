d8af29db473e133bcf85c950089427a97ca71851
sams <sams@8325946a-aa55-0410-8760-f0bc8c33efaa>
2009-03-19 15:02:33 +0000

    camelot
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@363 8325946a-aa55-0410-8760-f0bc8c33efaa

--- camelot/view/elixir_admin.py
+++ camelot/view/elixir_admin.py
@@ -72,7 +72,7 @@ class EntityAdmin(object):
   list_actions = []
   list_search = []
   list_size = (600, 400)
-  #form_size = (700, 500)
+  form_size = (700, 500)
   form_actions = []
   form_title_column = None
   field_attributes = {}
