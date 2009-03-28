9fdbb28bd3e497d62acacf05eadd7489ec6c3c0b
erikj <erikj@8325946a-aa55-0410-8760-f0bc8c33efaa>
2009-03-28 14:34:59 +0000

    test should not be under version control
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@403 8325946a-aa55-0410-8760-f0bc8c33efaa

--- camelot/empty_project/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Some boilerplate code for an empty project
-
-This project is copied by the camelot-admin tool
-when starting a new project
-"""
--- camelot/empty_project/main.py
+++
@@ -1,9 +0,0 @@
-import settings
-import logging
-
-logger = logging.getLogger('main')
-
-if __name__ == '__main__':
-  from camelot.view.main import main
-  from application_admin import MyApplicationAdmin
-  main(MyApplicationAdmin())
