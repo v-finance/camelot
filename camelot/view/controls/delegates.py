6803f94dfa3a7f279525a9915f64be8bacd8ead1
erikj <erikj@8325946a-aa55-0410-8760-f0bc8c33efaa>
2009-04-11 15:41:59 +0000

    - apparently, it's impossible to replace autofillbackground with a delegate that checks the state of the cell
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@427 8325946a-aa55-0410-8760-f0bc8c33efaa

--- camelot/view/controls/delegates.py
+++ camelot/view/controls/delegates.py
@@ -689,6 +689,14 @@ class Many2OneColumnDelegate(QtGui.QItemDelegate):
     self._kwargs = kwargs
     self._dummy_editor = editors.Many2OneEditor(self.admin, None)
 
+#  def paint(self, painter, option, index):
+#    painter.save()
+#    self.drawBackground(painter, option, index)
+#    print option.state, '%08x'%(option.state)
+#    if not (option.state & QtGui.QStyle.State_Editing):
+#      self.drawDisplay(painter, option, option.rect, index.data(Qt.DisplayRole).toString())
+#    painter.restore()
+    
   def createEditor(self, parent, option, index):
     if self._embedded:
       editor = editors.EmbeddedMany2OneEditor(self.admin, parent)
