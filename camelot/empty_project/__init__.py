78926f7dfb4acde810c22d0b44c6fef4f0638007
erikj <erikj@8325946a-aa55-0410-8760-f0bc8c33efaa>
2009-04-21 21:04:31 +0000

    undo removal of empty project
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@431 8325946a-aa55-0410-8760-f0bc8c33efaa

---
+++ camelot/empty_project/__init__.py
@@ -0,0 +1,5 @@
+"""Some boilerplate code for an empty project
+
+This project is copied by the camelot-admin tool
+when starting a new project
+"""
---
+++ camelot/empty_project/model.py
@@ -0,0 +1,7 @@
+
+import camelot.types
+from camelot.model import metadata, Entity, Field, ManyToOne, OneToMany, Unicode, Date, Integer, using_options
+from camelot.view.elixir_admin import EntityAdmin
+from camelot.view.forms import *
+
+__metadata__ = metadata
