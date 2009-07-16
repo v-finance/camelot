ebaeee9f380e214cae29179aef341dcce4f4f7f2
erikj <erikj@8325946a-aa55-0410-8760-f0bc8c33efaa>
2009-07-16 13:57:33 +0000

    - adapt empty project to new style sections
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@541 8325946a-aa55-0410-8760-f0bc8c33efaa

--- camelot/empty_project/application_admin.py
+++ camelot/empty_project/application_admin.py
@@ -1,21 +1,17 @@
 from camelot.view.art import Icon
-from camelot.view.application_admin import ApplicationAdmin
+from camelot.admin.application_admin import ApplicationAdmin
+from camelot.admin.section import Section
 
 class MyApplicationAdmin(ApplicationAdmin):
   
-  def __init__(self):
-    icon_relations = Icon('tango/24x24/apps/system-users.png').fullpath()
-    icon_config = Icon('tango/24x24/categories/preferences-system.png').fullpath()
-
-    super(MyApplicationAdmin, self).__init__([
-      ('relations', ('Relations', icon_relations)),
-      ('configuration', ('Configuration', icon_config)),
-    ])
-
+  def get_sections(self):
     from camelot.model.memento import Memento
     from camelot.model.authentication import Person, Organization
     from camelot.model.i18n import Translation
-    self.register(Memento, Memento.Admin)
-    self.register(Person, Person.Admin)
-    self.register(Organization, Organization.Admin)
-    self.register(Translation, Translation.Admin)
+    return [Section('relation',
+                    Icon('tango/24x24/apps/system-users.png'),
+                    items = [Person, Organization]),
+            Section('configuration',
+                    Icon('tango/24x24/categories/preferences-system.png'),
+                    items = [Memento, Translation])
+            ]
