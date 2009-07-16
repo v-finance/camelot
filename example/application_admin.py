d8e1ea76c77c2614de04c839831411bd691078bd
erikj <erikj@8325946a-aa55-0410-8760-f0bc8c33efaa>
2009-07-16 13:55:59 +0000

    - adapt example to new style sections
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@540 8325946a-aa55-0410-8760-f0bc8c33efaa

--- example/application_admin.py
+++ example/application_admin.py
@@ -1,28 +1,27 @@
 from camelot.view.art import Icon
-from camelot.view.application_admin import ApplicationAdmin
+from camelot.admin.application_admin import ApplicationAdmin
+from camelot.admin.section import Section
 
 class MyApplicationAdmin(ApplicationAdmin):
   
   name = 'Camelot Video Store'
   
   def __init__(self):
-    icon_relations = Icon('tango/24x24/apps/system-users.png').fullpath()
-    icon_config = Icon('tango/24x24/categories/preferences-system.png').fullpath()
-    icon_movies = Icon('tango/24x24/mimetypes/x-office-presentation.png').fullpath()
-
-    super(MyApplicationAdmin, self).__init__([
-      ('movies', ('Movies', icon_movies)),
-      ('relations', ('Relations', icon_relations)),
-      ('configuration', ('Configuration', icon_config)),
-    ])
-
+    super(MyApplicationAdmin, self).__init__()
+    
+  def get_sections(self):
     from camelot.model.memento import Memento
     from camelot.model.authentication import Person, Organization
-    from camelot.model.i18n import Translation
-    self.register(Memento, Memento.Admin)
-    self.register(Person, Person.Admin)
-    self.register(Organization, Organization.Admin)
-    self.register(Translation, Translation.Admin)
+    from camelot.model.i18n import Translation    
     from example.model import Movie, Tag
-    self.register(Movie, Movie.Admin)
-    self.register(Tag, Tag.Admin)
+    return [Section('movies', 
+                    Icon('tango/24x24/mimetypes/x-office-presentation.png'),
+                    items = [Movie, Tag]),
+            Section('relation',
+                    Icon('tango/24x24/apps/system-users.png'),
+                    items = [Person, Organization]),
+            Section('configuration',
+                    Icon('tango/24x24/categories/preferences-system.png'),
+                    items = [Memento, Translation])
+            ]
+
