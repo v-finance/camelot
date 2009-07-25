8796bad7e31e08bed092a0ba9ab12c9e0a544bae
victorn <victorn@8325946a-aa55-0410-8760-f0bc8c33efaa>
2009-07-25 18:16:57 +0000

    - adds camelot_date_format
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@585 8325946a-aa55-0410-8760-f0bc8c33efaa

--- camelot/core/constants.py
+++ camelot/core/constants.py
@@ -1,8 +1,8 @@
-'''
+"""
 Created on Jul 19, 2009
 
 @author: tw55413
-'''
+"""
 
 camelot_maxint = 2147483647
 camelot_minint = -2147483648
@@ -10,3 +10,4 @@ camelot_maxfloat = 1e15
 camelot_minfloat = -1e15
 camelot_time_format = 'hh:mm'
 camelot_datetime_format = 'dd-MM-yyyy hh:mm'
+camelot_date_format = 'dd-MM-yyyy'
