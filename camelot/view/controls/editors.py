5deb58938ce8e01e1f01bed31603baa7f453de3d
erikj <erikj@8325946a-aa55-0410-8760-f0bc8c33efaa>
2009-07-26 11:04:16 +0000

    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@591 8325946a-aa55-0410-8760-f0bc8c33efaa

--- camelot/view/controls/editors.py
+++ camelot/view/controls/editors.py
@@ -194,13 +194,14 @@ class ChoicesEditor(QtGui.QComboBox, AbstractCustomEditor):
 class OneToManyChoicesEditor(ChoicesEditor):
   
   def __init__(self, parent, editable=True, target=None, **kwargs):
+    ChoicesEditor.__init__(self, parent, editable, **kwargs)
     assert target!=None
     
     def get_choices():
       return [(o, unicode(o)) for o in target.query.all()]
         
-    ChoicesEditor.__init__(self, parent, editable, choices=get_choices, **kwargs)
-    self.setEnabled(editable)
+    from camelot.view.model_thread import get_model_thread
+    get_model_thread().post(get_choices, self.set_choices)    
         
 class DateTimeEditor(CustomEditor):
   """Widget for editing date and time separated and with popups"""
