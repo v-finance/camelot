9e610e85cb3dde08d51846b4bfab9e0e6e6a2716
victorn <victorn@8325946a-aa55-0410-8760-f0bc8c33efaa>
2009-07-28 21:45:12 +0000

    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@612 8325946a-aa55-0410-8760-f0bc8c33efaa

--- camelot/view/controls/editors/textlineeditor.py
+++ camelot/view/controls/editors/textlineeditor.py
@@ -2,7 +2,7 @@
 from customeditor import *
 
 class TextLineEditor(QtGui.QLineEdit, AbstractCustomEditor):
- 
+
   def __init__(self, parent, length, editable=True, **kwargs):
     QtGui.QLineEdit.__init__(self, parent)
     AbstractCustomEditor.__init__(self)
@@ -10,13 +10,13 @@ class TextLineEditor(QtGui.QLineEdit, AbstractCustomEditor):
       self.setMaxLength(length)
     if not editable:
       self.setEnabled(False)
-    
+
   def set_value(self, value):
     value = AbstractCustomEditor.set_value(self, value)
     if value:
       self.setText(unicode(value))
     else:
       self.setText('')
-      
+
   def get_value(self):
     return AbstractCustomEditor.get_value(self) or unicode(self.text())
