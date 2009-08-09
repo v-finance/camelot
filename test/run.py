cdee3ed517873bc0ea29378c5ed13a2e31a226a3
erikj <erikj@8325946a-aa55-0410-8760-f0bc8c33efaa>
2009-08-09 10:23:43 +0000

    add unit test for table view and navigation pane
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@675 8325946a-aa55-0410-8760-f0bc8c33efaa

--- test/run.py
+++ test/run.py
@@ -7,6 +7,7 @@ import unittest
 import sys
 import os
 
+from camelot.test import *
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 
@@ -24,16 +25,47 @@ def create_getter(getable):
 class ModelThreadTests(unittest.TestCase):
   """Base class for implementing test cases that need a running model_thread"""
   
+  def grab_widget(self, widget, suffix=None):
+    """Save the widget as a png file :
+
+- the name of the png file is the name of the test case, without 'test'
+- it is stored in the directory with the same name as the class, without 'test'     
+    """
+    import sys
+    self.process()
+    widget.adjustSize()
+    pixmap = QPixmap.grabWidget(widget)
+    # TODO checks if path exists
+    images_path = os.path.join(static_images_path, self.__class__.__name__.lower()[:-len('Test')])
+    if not os.path.exists(images_path):
+      os.makedirs(images_path)
+    test_case_name = sys._getframe(1).f_code.co_name[5:]
+    image_name = '%s.png'%test_case_name
+    if suffix:
+      image_name = '%s_%s.png'%(test_case_name, suffix)
+    pixmap.save(os.path.join(images_path, image_name), 'PNG')
+    
+  def process(self):
+    """Wait until all events are processed and the queues of the model thread are empty"""
+    self.mt.post_and_block(lambda:None)
+    print self.mt._request_queue.empty()
+    print self.mt._response_queue.empty()
+    self.app.processEvents()
+    self.mt.process_responses()
+    self.app.processEvents()
+
   def setUp(self):
+    from camelot.test import get_application
+    self.app = get_application()
     from camelot.view.model_thread import get_model_thread, construct_model_thread
     from camelot.view.response_handler import ResponseHandler
     from camelot.view.remote_signals import construct_signal_handler
     rh = ResponseHandler()
     construct_model_thread(rh)
     construct_signal_handler()
-    construct_model_thread(rh)
     self.mt = get_model_thread()
-    self.mt.start()
+    if not self.mt.ident:
+      self.mt.start()
     
   def tearDown(self):
     self.mt.exit()
@@ -801,13 +833,25 @@ class DelegateTest(unittest.TestCase):
 
 class ControlsTest(ModelThreadTests):
   
-  def testTableView(self):
-    pass
+  def setUp(self):
+    super(ControlsTest, self).setUp()
+    from camelot.admin.application_admin import ApplicationAdmin
+    self.app_admin = ApplicationAdmin()
+        
+  def test_table_view(self):
+    from camelot.view.controls.tableview import TableView
+    from camelot.model.authentication import Person
+    widget = TableView(self.app_admin.getEntityAdmin(Person))
+    self.grab_widget(widget)
+  
+  def test_navigation_pane(self):
+    from camelot.view.controls import navpane
+    widget = navpane.NavigationPane(self.app_admin)
+    self.grab_widget(widget)
     
 if __name__ == '__main__':
   logger.info('running unit tests')
-  import sys
-  app = QApplication(sys.argv)
+  app = get_application()
   
   editor_test =  unittest.makeSuite(EditorTest, 'test')
   runner=unittest.TextTestRunner(verbosity=2)
@@ -817,5 +861,3 @@ if __name__ == '__main__':
   runner.run(editor_test)
   controls_test = unittest.makeSuite(ControlsTest, 'test')
   runner.run(controls_test)
-    
-  #sys.exit()
