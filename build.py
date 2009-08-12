ef81a4fbe4a37acf0b12d0c317b9216df3e0b5b7
erikj <erikj@8325946a-aa55-0410-8760-f0bc8c33efaa>
2009-08-12 20:45:26 +0000

    make sure to rebuild all documentation
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@693 8325946a-aa55-0410-8760-f0bc8c33efaa

--- build.py
+++ build.py
@@ -2,13 +2,13 @@
 import runpy
 import sys
 import os
-sys.argv += ['-style', 'plastique']
+sys.argv += ['-style', 'cleanlooks']
 sys.path.append( os.path.join(os.path.dirname(__file__), 'test') )
 runpy.run_module('test.run', run_name='__main__')
 del sys.argv[-2:]
 
 import sphinx
-sphinx.main(['sphinx-build', '-a', 'doc/sphinx/source', 'doc/sphinx/build',])
+sphinx.main(['sphinx-build', '-a', '-E', 'doc/sphinx/source', 'doc/sphinx/build',])
 
 import sys
 sys.argv += ['sdist', 'bdist_egg']
