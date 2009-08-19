d0c07d71a020c97e2a4059b750d72b15e1d75061
erikdr <erikdr@8325946a-aa55-0410-8760-f0bc8c33efaa>
2009-08-19 09:32:20 +0000

    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@709 8325946a-aa55-0410-8760-f0bc8c33efaa

---
+++ startBuildOut.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+ELEXIR_LIB_PATH=""
+SQLALCHEMY_LIB_PATH=""
+XVFB_DISPLAY=":99"
+XVFB_SCREEN="0"
+set -e
+if [ `id -u` = 1000 ]; then
+        echo "Running as test user. Good."
+else   
+        echo "This script must be run as test user (uid=1000) in a chroot (/var/chroot)";
+        exit 1
+fi
+svn co http://www.conceptive.be/camelot/svn/trunk --username guest --password guest --non-interactive
+cd /home/test/camelotTesting/trunk
+echo "Checking if Xvfb is running..."
+if [ -n "`ps -e | grep Xvfb`" ]; then
+        echo "Xvfb is running. Good."
+else
+        echo "Xvfb is not running."
+        echo "Checking if Xvfb is installed..."
+        if which Xvfb >/dev/null; then
+                echo "Xvfb is installed. Good."
+                echo "Staring Xvfb on DISPLAY $XVFB_DISPLAY on screen $XVFB_SCREEN".
+                Xvfb "$XVFB_DISPLAY" -ac -screen "$XVFB_SCREEN" 1024x768x24&
+        else
+                echo "Xvfb is not installed or this script was unable to find it."
+                exit 1
+        fi
+fi
+export DISPLAY="$XVFB_DISPLAY"
+export PYTHONPATH=".:../libraries"
+python /home/test/camelotTesting/trunk/build.py
+python /home/test/camelotTesting/testBuildOut.py
