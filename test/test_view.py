30a31f4c509b640a3714550c4bc1bbf7fb3e6f88
erikj <erikj@8325946a-aa55-0410-8760-f0bc8c33efaa>
2009-08-22 11:37:27 +0000

    rename run to test_view to make it nose compatible
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@722 8325946a-aa55-0410-8760-f0bc8c33efaa

--- test/test_view.py
+++ test/test_view.py
@@ -21,186 +21,148 @@ def create_getter(getable):
     return getable
   
   return getter
-      
-def testSuites():
-
-  from camelot.view.model_thread import get_model_thread, construct_model_thread
-  from camelot.view.response_handler import ResponseHandler
-  from camelot.view.remote_signals import construct_signal_handler
-  rh = ResponseHandler()
-  construct_model_thread(rh)
-  construct_signal_handler()
-  construct_model_thread(rh)
-  get_model_thread().start()
-  
-#  class model_thread_tests(unittest.TestCase):
-#      def setUp(self):
-#        self.mt = get_model_thread()
-#      def testRequestAndResponseInDifferentThread(self):
-#        """Test if the request function is really executed in another thread"""
-#        
-#        def get_request_thread():
-#          import threading
-#          return threading.currentThread()
-#        
-#        def compare_with_response_thread(thread):
-#          import threading
-#          self.assertNotEqual(thread, threading.currentThread())
-#          
-#        event = self.mt.post( get_request_thread, compare_with_response_thread )
-#        event.wait()
-#        self.mt.process_responses()
         
-  class ProxyEntityTest(unittest.TestCase):
-    """Test the functionality of the proxies to perform CRUD operations on stand
-    alone data"""
-    def setUp(self):
-      from camelot.model.authentication import Person, PartyAddressRoleType
-      from camelot.view.proxy.queryproxy import QueryTableProxy
-      from camelot.view.application_admin import ApplicationAdmin
-      self.app_admin = ApplicationAdmin()
-      self.mt = get_model_thread()
-      self.block = self.mt.post_and_block
-      person_admin = self.app_admin.getEntityAdmin(Person)
-      party_address_role_type_admin = self.app_admin.getEntityAdmin(PartyAddressRoleType)
-      self.person_proxy = QueryTableProxy(person_admin, lambda:Person.query, person_admin.getFields)
-      self.party_address_role_type_proxy = QueryTableProxy(party_address_role_type_admin, lambda:PartyAddressRoleType.query, party_address_role_type_admin.getFields)
-      # get the columns of the proxy
-      self.person_columns = dict((c[0],i) for i,c in enumerate(self.block(lambda:self.person_proxy.getColumns())))
-      self.rows_before_insert = 0
-      self.rows_after_insert = 0
-      self.rows_after_delete = 0
-      self.new_person = None
-      # make sure nothing is running in the model thread any more
-      self.block(lambda:None)
-    def listPersons(self):
-      from camelot.model.authentication import Person
-      
-      def create_list():
-        print '<persons>'
-        for p in Person.query.all():
-          print p.id, unicode(p)
-        print '</persons>'
-      
-      self.block(create_list)
-    def numberOfPersons(self):
-      return self.block(lambda:self.person_proxy.getRowCount())
-    def insertNewPerson(self, valid):
-      from camelot.model.authentication import Person
-      self.rows_before_insert = self.numberOfPersons()
-      self.new_person = self.block(lambda:Person(first_name={True:u'test', False:None}[valid]))
-      self.person_proxy.insertRow(0, create_getter(self.new_person))
-      self.rows_after_insert = self.numberOfPersons()
-      self.assertTrue( self.rows_after_insert > self.rows_before_insert)
-    def updateNewPerson(self):
-      rows_before_update = self.numberOfPersons()
-      self.assertNotEqual( self.block(lambda:self.new_person.last_name), u'Testers')
-      index = self.person_proxy.index(self.rows_before_insert, self.person_columns['last_name'])
-      self.person_proxy.setData(index, lambda:u'Testers')
-      self.assertEqual( self.block(lambda:self.new_person.last_name), u'Testers')
-      self.assertEqual(rows_before_update, self.numberOfPersons())
-    def updateNewPersonToValid(self):
-      index = self.person_proxy.index(self.rows_before_insert, self.person_columns['first_name'])
-      self.person_proxy.setData(index, lambda:u'test')
-      index = self.person_proxy.index(self.rows_before_insert, self.person_columns['last_name'])
-      self.person_proxy.setData(index, lambda:u'test')      
-    def updateNewPersonToInvalid(self):
-      index = self.person_proxy.index(self.rows_before_insert, self.person_columns['first_name'])
-      self.person_proxy.setData(index, lambda:None)
-      index = self.person_proxy.index(self.rows_before_insert, self.person_columns['last_name'])
-      self.person_proxy.setData(index, lambda:None)      
-    def deleteNewPerson(self):
-      self.person_proxy.removeRow(self.rows_before_insert)
-      self.rows_after_delete = self.numberOfPersons()
-      self.assertTrue( self.rows_after_delete < self.rows_after_insert )
-    def testCreateDataWithoutRequiredFields(self):
-      """Create a record that has no required fields, so the creation
-      should be instantaneous as opposed to delayed until all required fields are filled"""
-      from camelot.model.authentication import PartyAddressRoleType
-      rows_before_insert = self.block(lambda:self.party_address_role_type_proxy.getRowCount())
-      new_party_address_role_type_proxy = self.block(lambda:PartyAddressRoleType())
-      self.party_address_role_type_proxy.insertRow(0, create_getter(new_party_address_role_type_proxy))
-      self.assertNotEqual( self.block(lambda:new_party_address_role_type_proxy.id), None )
-      rows_after_insert = self.block(lambda:self.party_address_role_type_proxy.getRowCount())
-      self.assertTrue( rows_after_insert > rows_before_insert)      
-    def testCreateUpdateDeleteValidData(self):
-      self.insertNewPerson(valid=True)
-      self.assertTrue( self.block(lambda:self.new_person.id) != None )
-      self.updateNewPerson()
-      self.deleteNewPerson()
-    def testCreateUpdateDeleteInvalidData(self):
-      self.insertNewPerson(valid=False)
-      self.assertTrue( self.block(lambda:self.new_person.id) == None )
-      self.updateNewPerson()
-      self.deleteNewPerson()
-    def testCreateInvalidDataUpdateToValidAndDelete(self):
-      self.insertNewPerson(valid=False)
-      self.assertTrue( self.block(lambda:self.new_person.id) == None )
-      self.updateNewPersonToValid()
-      self.assertTrue( self.block(lambda:self.new_person.id) != None )
-      self.deleteNewPerson()
-    def testCreateValidDataUpdateToInvalidAndDelete(self):
-      self.insertNewPerson(valid=False)
-      self.updateNewPersonToInvalid()
-      self.deleteNewPerson()
-                  
-  class ProxyOneToManyTest(ProxyEntityTest):
-    """Test the functionality of the proxies to perform CRUD operations on related
-    data"""
-    def setUp(self):
-      super(ProxyOneToManyTest, self).setUp()
-      from elixir import session
-      from camelot.view.proxy.queryproxy import QueryTableProxy
-      from camelot.model.authentication import Country, City, Address, PartyAddress
-      party_address_admin = self.app_admin.getEntityAdmin(PartyAddress)
-      self.party_address_proxy = QueryTableProxy(party_address_admin, PartyAddress.query, party_address_admin.getFields)
-      self.party_address_columns = dict((c[0],i) for i,c in enumerate(self.block(lambda:self.party_address_proxy.getColumns())))
-      self.country = self.block(lambda:Country(code=u'BE', name=u'Belgium'))
-      self.city = self.block(lambda:City(code=u'2000', name=u'Antwerp', country=self.country))
-      self.address = self.block(lambda:Address(street1=u'Teststreet', city=self.city))
-      self.block(lambda:session.flush([self.country, self.city, self.address]))
-    def insertRelatedPartyAddress(self, valid):
-      from camelot.model.authentication import PartyAddress
-      index = self.person_proxy.index(self.rows_before_insert, self.person_columns['addresses'])
-      self.block(lambda:None)
-      self.related_party_address_proxy = self.person_proxy.data(index, Qt.EditRole).toPyObject()
-      self.block(lambda:None)
-      self.related_party_address_proxy = self.person_proxy.data(index, Qt.EditRole).toPyObject()
-      self.assertNotEqual(self.related_party_address_proxy, None)
-      self.party_address = self.block({True:lambda:PartyAddress(address=self.address), False:lambda:PartyAddress()}[valid])
-      self.related_party_address_proxy.insertRow(0, create_getter(self.party_address))
-      self.block(lambda:None)
-    def testCreateBothValidDataUpdateDelete(self):
-      self.insertNewPerson(valid=True)
-      self.insertRelatedPartyAddress(valid=True)
-      self.assertNotEqual(self.party_address.id, None)
-      self.deleteNewPerson()
-    def testCreateValidPersonInvalidPartyAddressUpdateDelete(self):
-      self.insertNewPerson(valid=True)
-      self.insertRelatedPartyAddress(valid=False)
-      self.assertEqual(self.party_address.id, None)
-      self.deleteNewPerson()
-    def testCreateInvalidPersonValidPartyAddressUpdateDelete(self):
-      self.insertNewPerson(valid=False)
-      self.insertRelatedPartyAddress(valid=False)
-      self.assertEqual(self.party_address.id, None)
-      self.deleteNewPerson()
-#    def testCreateBothInvalidUpdateDelete(self):
-#      self.insertNewPerson(valid=False)
-#      self.insertRelatedPartyAddress(valid=False)
-#      #self.assertEqual(self.party_address.id, None)
-#      self.deleteNewPerson()
-    def tearDown(self):
-      from elixir import session
-      self.block(lambda:self.address.delete())
-      self.block(lambda:self.city.delete())
-      self.block(lambda:self.country.delete())
-      self.block(lambda:session.flush([self.country, self.city, self.address]))
+class ProxyEntityTest(ModelThreadTestCase):
+  """Test the functionality of the proxies to perform CRUD operations on stand
+  alone data"""
+  def setUp(self):
+    ModelThreadTestCase.setUp(self)
+    from camelot.model.authentication import Person, PartyAddressRoleType
+    from camelot.view.proxy.queryproxy import QueryTableProxy
+    from camelot.view.application_admin import ApplicationAdmin
+    self.app_admin = ApplicationAdmin()
+    self.block = self.mt.post_and_block
+    person_admin = self.app_admin.getEntityAdmin(Person)
+    party_address_role_type_admin = self.app_admin.getEntityAdmin(PartyAddressRoleType)
+    self.person_proxy = QueryTableProxy(person_admin, lambda:Person.query, person_admin.getFields)
+    self.party_address_role_type_proxy = QueryTableProxy(party_address_role_type_admin, lambda:PartyAddressRoleType.query, party_address_role_type_admin.getFields)
+    # get the columns of the proxy
+    self.person_columns = dict((c[0],i) for i,c in enumerate(self.block(lambda:self.person_proxy.getColumns())))
+    self.rows_before_insert = 0
+    self.rows_after_insert = 0
+    self.rows_after_delete = 0
+    self.new_person = None
+    # make sure nothing is running in the model thread any more
+    self.block(lambda:None)
+  def listPersons(self):
+    from camelot.model.authentication import Person
     
-  import inspect
-  for c in locals().values():
-    if inspect.isclass(c):
-      yield unittest.makeSuite(c, 'test')
+    def create_list():
+      print '<persons>'
+      for p in Person.query.all():
+        print p.id, unicode(p)
+      print '</persons>'
+    
+    self.block(create_list)
+  def numberOfPersons(self):
+    return self.block(lambda:self.person_proxy.getRowCount())
+  def insertNewPerson(self, valid):
+    from camelot.model.authentication import Person
+    self.rows_before_insert = self.numberOfPersons()
+    self.new_person = self.block(lambda:Person(first_name={True:u'test', False:None}[valid], last_name='test'))
+    self.person_proxy.insertRow(0, create_getter(self.new_person))
+    self.rows_after_insert = self.numberOfPersons()
+    self.assertTrue( self.rows_after_insert > self.rows_before_insert)
+  def updateNewPerson(self):
+    rows_before_update = self.numberOfPersons()
+    self.assertNotEqual( self.block(lambda:self.new_person.last_name), u'Testers')
+    index = self.person_proxy.index(self.rows_before_insert, self.person_columns['last_name'])
+    self.person_proxy.setData(index, lambda:u'Testers')
+    self.assertEqual( self.block(lambda:self.new_person.last_name), u'Testers')
+    self.assertEqual(rows_before_update, self.numberOfPersons())
+  def updateNewPersonToValid(self):
+    index = self.person_proxy.index(self.rows_before_insert, self.person_columns['first_name'])
+    self.person_proxy.setData(index, lambda:u'test')
+    index = self.person_proxy.index(self.rows_before_insert, self.person_columns['last_name'])
+    self.person_proxy.setData(index, lambda:u'test')      
+  def updateNewPersonToInvalid(self):
+    index = self.person_proxy.index(self.rows_before_insert, self.person_columns['first_name'])
+    self.person_proxy.setData(index, lambda:None)
+    index = self.person_proxy.index(self.rows_before_insert, self.person_columns['last_name'])
+    self.person_proxy.setData(index, lambda:None)      
+  def deleteNewPerson(self):
+    self.person_proxy.removeRow(self.rows_before_insert)
+    self.rows_after_delete = self.numberOfPersons()
+    self.assertTrue( self.rows_after_delete < self.rows_after_insert )
+  def testCreateDataWithoutRequiredFields(self):
+    """Create a record that has no required fields, so the creation
+    should be instantaneous as opposed to delayed until all required fields are filled"""
+    from camelot.model.authentication import PartyAddressRoleType
+    rows_before_insert = self.block(lambda:self.party_address_role_type_proxy.getRowCount())
+    new_party_address_role_type_proxy = self.block(lambda:PartyAddressRoleType())
+    self.party_address_role_type_proxy.insertRow(0, create_getter(new_party_address_role_type_proxy))
+    self.assertNotEqual( self.block(lambda:new_party_address_role_type_proxy.id), None )
+    rows_after_insert = self.block(lambda:self.party_address_role_type_proxy.getRowCount())
+    self.assertTrue( rows_after_insert > rows_before_insert)      
+  def testCreateUpdateDeleteValidData(self):
+    self.insertNewPerson(valid=True)
+    self.assertTrue( self.block(lambda:self.new_person.id) != None )
+    self.updateNewPerson()
+    self.deleteNewPerson()
+  def testCreateUpdateDeleteInvalidData(self):
+    self.insertNewPerson(valid=False)
+    self.assertTrue( self.block(lambda:self.new_person.id) == None )
+    self.updateNewPerson()
+    self.deleteNewPerson()
+  def testCreateInvalidDataUpdateToValidAndDelete(self):
+    self.insertNewPerson(valid=False)
+    self.assertTrue( self.block(lambda:self.new_person.id) == None )
+    self.updateNewPersonToValid()
+    self.assertTrue( self.block(lambda:self.new_person.id) != None )
+    self.deleteNewPerson()
+  def testCreateValidDataUpdateToInvalidAndDelete(self):
+    self.insertNewPerson(valid=False)
+    self.updateNewPersonToInvalid()
+    self.deleteNewPerson()
+
+                
+#class ProxyOneToManyTest(ProxyEntityTest):
+#  """Test the functionality of the proxies to perform CRUD operations on related
+#  data"""
+#  def setUp(self):
+#    super(ProxyOneToManyTest, self).setUp()
+#    from elixir import session
+#    from camelot.view.proxy.queryproxy import QueryTableProxy
+#    from camelot.model.authentication import Country, City, Address, PartyAddress
+#    party_address_admin = self.app_admin.getEntityAdmin(PartyAddress)
+#    self.party_address_proxy = QueryTableProxy(party_address_admin, PartyAddress.query, party_address_admin.getFields)
+#    self.party_address_columns = dict((c[0],i) for i,c in enumerate(self.block(lambda:self.party_address_proxy.getColumns())))
+#    self.country = self.block(lambda:Country(code=u'BE', name=u'Belgium'))
+#    self.city = self.block(lambda:City(code=u'2000', name=u'Antwerp', country=self.country))
+#    self.address = self.block(lambda:Address(street1=u'Teststreet', city=self.city))
+#    self.block(lambda:session.flush([self.country, self.city, self.address]))
+#  def insertRelatedPartyAddress(self, valid):
+#    from camelot.model.authentication import PartyAddress
+#    index = self.person_proxy.index(self.rows_before_insert, self.person_columns['addresses'])
+#    self.block(lambda:None)
+#    self.related_party_address_proxy = self.person_proxy.data(index, Qt.EditRole).toPyObject()
+#    self.block(lambda:None)
+#    self.related_party_address_proxy = self.person_proxy.data(index, Qt.EditRole).toPyObject()
+#    self.assertNotEqual(self.related_party_address_proxy, None)
+#    self.party_address = self.block({True:lambda:PartyAddress(address=self.address), False:lambda:PartyAddress()}[valid])
+#    self.related_party_address_proxy.insertRow(0, create_getter(self.party_address))
+#    self.block(lambda:None)
+#  def testCreateBothValidDataUpdateDelete(self):
+#    self.insertNewPerson(valid=True)
+#    self.insertRelatedPartyAddress(valid=True)
+#    self.assertNotEqual(self.party_address.id, None)
+#    self.deleteNewPerson()
+#  def testCreateValidPersonInvalidPartyAddressUpdateDelete(self):
+#    self.insertNewPerson(valid=True)
+#    self.insertRelatedPartyAddress(valid=False)
+#    self.assertEqual(self.party_address.id, None)
+#    self.deleteNewPerson()
+#  def testCreateInvalidPersonValidPartyAddressUpdateDelete(self):
+#    self.insertNewPerson(valid=False)
+#    self.insertRelatedPartyAddress(valid=False)
+#    self.assertEqual(self.party_address.id, None)
+#    self.deleteNewPerson()
+#  def tearDown(self):
+#    from elixir import session
+#    self.block(lambda:self.address.delete())
+#    self.block(lambda:self.city.delete())
+#    self.block(lambda:self.country.delete())
+#    self.block(lambda:session.flush([self.country, self.city, self.address]))
       
 class EditorTest(unittest.TestCase):
   """
@@ -879,7 +841,10 @@ if __name__ == '__main__':
   app = get_application()
 
   runner=unittest.TextTestRunner(verbosity=2)
-
+  proxy_test =  unittest.makeSuite(ProxyEntityTest, 'test')
+  runner.run(proxy_test)
+#  proxy_one_to_many_test =  unittest.makeSuite(ProxyOneToManyTest, 'test')
+#  runner.run(proxy_one_to_many_test)
   editor_test =  unittest.makeSuite(EditorTest, 'test')
   runner.run(editor_test)
   editor_test =  unittest.makeSuite(DelegateTest, 'test')
