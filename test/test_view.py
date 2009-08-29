679886fd822e43437a7a446e53fc881f3efed171
erikj <erikj@8325946a-aa55-0410-8760-f0bc8c33efaa>
2009-08-29 12:24:36 +0000

    add schema test
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@769 8325946a-aa55-0410-8760-f0bc8c33efaa

--- test/test_view.py
+++ test/test_view.py
@@ -846,15 +846,21 @@ class CamelotEntityViewsTest(EntityViewsTest):
   
   images_path = static_images_path
   
+class CamelotSchemaTest(SchemaTest):
+  
+  images_path = static_images_path
+  
 if __name__ == '__main__':
   logger.info('running unit tests')
   app = get_application()
 
   runner=unittest.TextTestRunner(verbosity=2)
-  proxy_test =  unittest.makeSuite(ProxyEntityTest, 'test')
-  runner.run(proxy_test)
+#  proxy_test =  unittest.makeSuite(ProxyEntityTest, 'test')
+#  runner.run(proxy_test)
 #  proxy_one_to_many_test =  unittest.makeSuite(ProxyOneToManyTest, 'test')
 #  runner.run(proxy_one_to_many_test)
+  schema_test =  unittest.makeSuite(CamelotSchemaTest, 'test')
+  runner.run(schema_test)
   form_action_test =  unittest.makeSuite(FormActionTest, 'test')
   runner.run(form_action_test)
   editors_test =  unittest.makeSuite(EditorsTest, 'test')
