10438da888e7bc9f53e964aed583d2cb270db55b
sams <sams@8325946a-aa55-0410-8760-f0bc8c33efaa>
2009-08-31 07:17:13 +0000

    ComboBoxDelegate Paint method gemaakt
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@773 8325946a-aa55-0410-8760-f0bc8c33efaa

--- camelot/view/controls/delegates/comboboxdelegate.py
+++ camelot/view/controls/delegates/comboboxdelegate.py
@@ -10,6 +10,7 @@ class ComboBoxDelegate(CustomDelegate):
   def __init__(self, parent, choices, editable=True, **kwargs):
     CustomDelegate.__init__(self, parent, editable=editable, **kwargs)
     self.choices = choices
+
               
   def setEditorData(self, editor, index):
     from camelot.core.utils import variant_to_pyobject
@@ -27,3 +28,58 @@ class ComboBoxDelegate(CustomDelegate):
     get_model_thread().post(create_choices_getter(index.model(),
                                                   index.row()),
                                                   editor.set_choices)
+
+  def paint(self, painter, option, index):
+    painter.save()
+    self.drawBackground(painter, option, index)
+    value = variant_to_pyobject(index.data(Qt.EditRole))
+    
+    
+    editor = editors.ChoicesEditor(parent=None)
+    
+    background_color = QtGui.QColor(index.model().data(index, Qt.BackgroundRole))
+    
+    rect = option.rect
+    rect = QtCore.QRect(rect.left() + 3, rect.top() + 6, rect.width() - 5, rect.height())
+    
+    if(option.state & QtGui.QStyle.State_Selected):
+        painter.fillRect(option.rect, option.palette.highlight())
+        fontColor = QtGui.QColor()
+        if self.editable:         
+          Color = option.palette.highlightedText().color()
+          fontColor.setRgb(Color.red(), Color.green(), Color.blue())
+        else:          
+          fontColor.setRgb(130, 130, 130)
+    else:
+        if self.editable:
+          painter.fillRect(option.rect, background_color)
+          fontColor = QtGui.QColor()
+          fontColor.setRgb(0, 0, 0)
+        else:
+          painter.fillRect(option.rect, option.palette.window())
+          fontColor = QtGui.QColor()
+          fontColor.setRgb(130, 130, 130)
+          
+    
+    choice = ''
+    for i, (number, name) in enumerate(self.choices):
+      if int(number) == int(value):
+        choice = str(name)
+            
+    
+    
+    text = choice
+    
+    
+    painter.setPen(fontColor.toRgb())
+    rect = QtCore.QRect(option.rect.left()+2,
+                        option.rect.top(),
+                        option.rect.width()-2,
+                        option.rect.height())
+    painter.drawText(rect.x(),
+                     rect.y(),
+                     rect.width(),
+                     rect.height(),
+                     Qt.AlignVCenter | Qt.AlignLeft,
+                     unicode(text))
+    painter.restore()
