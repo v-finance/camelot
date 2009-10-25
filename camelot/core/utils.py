1a2087014ffb580429384b55f91b94afcf0a190e
victorn <victorn@8325946a-aa55-0410-8760-f0bc8c33efaa>
2009-10-25 04:49:21 +0000

    enforces pep8
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@908 8325946a-aa55-0410-8760-f0bc8c33efaa

--- camelot/core/utils.py
+++ camelot/core/utils.py
@@ -28,9 +28,12 @@
 """Utility functions"""
 
 from PyQt4 import QtCore
+from PyQt4.QtCore.QCoreApplication import translate
+
 
 def create_constant_function(constant):
   return lambda:constant
+
 
 def variant_to_pyobject(qvariant=None):
     """Try to convert a QVariant to a python object as good
@@ -39,33 +42,33 @@ def variant_to_pyobject(qvariant=None):
     if not qvariant:
         return None
     if qvariant.isNull():
-      return None
+        return None
     type = qvariant.type()
     if type == QtCore.QVariant.String:
-      value = unicode(qvariant.toString())
+        value = unicode(qvariant.toString())
     elif type == QtCore.QVariant.Date:
-      value = qvariant.toDate()
-      value = datetime.date(year=value.year(),
-                            month=value.month(),
-                            day=value.day())
+        value = qvariant.toDate()
+        value = datetime.date(year=value.year(),
+                              month=value.month(),
+                              day=value.day())
     elif type == QtCore.QVariant.Int:
-      value = int(qvariant.toInt()[0])
+        value = int(qvariant.toInt()[0])
     elif type == QtCore.QVariant.LongLong:
-      value = int(qvariant.toLongLong()[0])      
+        value = int(qvariant.toLongLong()[0])      
     elif type == QtCore.QVariant.Double:
-      value = float(qvariant.toDouble()[0])
+        value = float(qvariant.toDouble()[0])
     elif type == QtCore.QVariant.Bool:
-      value = bool(qvariant.toBool())
+        value = bool(qvariant.toBool())
     elif type == QtCore.QVariant.Time:
         value = qvariant.toTime()
         value = datetime.time(hour = value.hour(),
                               minute = value.minute(),
                               second = value.second())
     elif type == QtCore.QVariant.DateTime:
-      value = qvariant.toDateTime()
-      value = value.toPyDateTime ()
+        value = qvariant.toDateTime()
+        value = value.toPyDateTime ()
     else:
-      value = qvariant.toPyObject()
+        value = qvariant.toPyObject()
       
     return value
 
@@ -80,28 +83,30 @@ def load_translations():
     database, to be able to do fast gui thread lookups of translations"""
     language = unicode(QtCore.QLocale().name())
     from camelot.model.i18n import Translation
-    for t in Translation.query.filter(Translation.language==language).filter(Translation.value!=None).all():
+    translations = Translation.query.filter(Translation.language==language)
+    translations = translations.filter(Translation.value!=None).all()
+    for t in translations:
         if t.value:
             _translations_[t.source] = t.value
   
 def ugettext(string_to_translate):
-    """Translate the string_to_translate to the language of the current locale.  This
-    is a two step process.  First the function will try to get the translation out
-    of the Translation entity, if this is not successfull, the function will ask
-    QCoreApplication to translate string_to_translate 
+    """Translate the string_to_translate to the language of the current locale.
+    This is a two step process.  First the function will try to get the
+    translation out of the Translation entity, if this is not successfull, the
+    function will ask QCoreApplication to translate string_to_translate 
     (which tries to get the translation from the .po files)"""
     result = _translations_.get(string_to_translate, None)
     if not result:
-        result = unicode(QtCore.QCoreApplication.translate('', string_to_translate))
+        result = unicode(translate('', string_to_translate))
     return result
   
 class ugettext_lazy(object):
   
   def __init__(self, string_to_translate):
-    self._string_to_translate = string_to_translate
+      self._string_to_translate = string_to_translate
     
   def __str__(self):
-    return ugettext(self._string_to_translate)
+      return ugettext(self._string_to_translate)
   
   def __unicode__(self):
-    return ugettext(self._string_to_translate)
+      return ugettext(self._string_to_translate)
