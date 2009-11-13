a29ed12527a02d4573e0fc15b6d87519ad279cfc
erikdr <erikdr@8325946a-aa55-0410-8760-f0bc8c33efaa>
2009-11-13 09:41:43 +0000

    changed assertion from 'admin' to 'delegate' (wrong assertion?)
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@954 8325946a-aa55-0410-8760-f0bc8c33efaa

--- camelot/view/controls/delegates/one2manydelegate.py
+++ camelot/view/controls/delegates/one2manydelegate.py
@@ -5,30 +5,30 @@ from camelot.view.controls import editors
 from camelot.core.utils import variant_to_pyobject
 
 import logging
-logger = logging.getLogger('camelot.view.controls.delegates.one2manydelegate')
+logger = logging.getLogger( 'camelot.view.controls.delegates.one2manydelegate' )
 
-class One2ManyDelegate(QtGui.QItemDelegate):
+class One2ManyDelegate( QtGui.QItemDelegate ):
     """Custom delegate for many 2 one relations
   
   .. image:: ../_static/onetomany.png  
   """
-  
-    def __init__(self, parent=None, **kwargs):
-        logger.debug('create one2manycolumn delegate')
-        assert 'admin' in kwargs
-        QtGui.QItemDelegate.__init__(self, parent)
+
+    def __init__( self, parent = None, **kwargs ):
+        logger.debug( 'create one2manycolumn delegate' )
+        assert 'delegate' in kwargs
+        QtGui.QItemDelegate.__init__( self, parent )
         self.kwargs = kwargs
-    
-    def createEditor(self, parent, option, index):
-        logger.debug('create a one2many editor')
-        editor = editors.One2ManyEditor(parent=parent, **self.kwargs)
-        self.setEditorData(editor, index)
+
+    def createEditor( self, parent, option, index ):
+        logger.debug( 'create a one2many editor' )
+        editor = editors.One2ManyEditor( parent = parent, **self.kwargs )
+        self.setEditorData( editor, index )
         return editor
-    
-    def setEditorData(self, editor, index):
-        logger.debug('set one2many editor data')
-        model = variant_to_pyobject(index.data(Qt.EditRole))
-        editor.set_value(model)
-    
-    def setModelData(self, editor, model, index):
+
+    def setEditorData( self, editor, index ):
+        logger.debug( 'set one2many editor data' )
+        model = variant_to_pyobject( index.data( Qt.EditRole ) )
+        editor.set_value( model )
+
+    def setModelData( self, editor, model, index ):
         pass
