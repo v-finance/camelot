bff600995d8bc8a70fed5d314a8617c97a148c64
erikj <erikj@8325946a-aa55-0410-8760-f0bc8c33efaa>
2009-11-21 16:05:27 +0000

    convert example to new style setup model
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@995 8325946a-aa55-0410-8760-f0bc8c33efaa

--- example/settings.py
+++ example/settings.py
@@ -1,6 +1,4 @@
 import logging
-import inspect
-import camelot
 import os
 
 FORMAT = '[%(levelname)-7s] [%(name)-35s] - %(message)s' 
@@ -13,10 +11,9 @@ ENGINE = lambda:'sqlite:///videostore.sqlite'
 #logging.getLogger('camelot.view.proxy.collection_proxy').setLevel(logging.DEBUG)
 
 def setup_model():
-  from example.model import Movie, Cast
-  from camelot.model.memento import *
-  from camelot.model.synchronization import *
-  from camelot.model.authentication import *
-  from camelot.model.i18n import *
-  setup_all(create_tables=True)
-  updateLastLogin()
+    import camelot.model
+    import example.model
+    from elixir import setup_all
+    setup_all(create_tables=True)
+    from camelot.model.authentication import updateLastLogin
+    updateLastLogin()
