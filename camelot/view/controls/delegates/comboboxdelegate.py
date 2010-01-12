d54aabb12db2b163880de2401c8d61bb8254c00d
erikj <erikj@8325946a-aa55-0410-8760-f0bc8c33efaa>
2010-01-12 17:21:20 +0000

    use DisplayRole to paint text of comboboxdelegate
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@1085 8325946a-aa55-0410-8760-f0bc8c33efaa

--- camelot/view/controls/delegates/comboboxdelegate.py
+++ camelot/view/controls/delegates/comboboxdelegate.py
@@ -61,7 +61,7 @@ class ComboBoxDelegate(CustomDelegate):
     def paint(self, painter, option, index):
         painter.save()
         self.drawBackground(painter, option, index)
-        value = variant_to_pyobject(index.data(Qt.EditRole))
+        value = variant_to_pyobject(index.data(Qt.DisplayRole))
         if value in (None, ValueLoading):
             value = ''
         c = index.model().data(index, Qt.BackgroundRole)
@@ -116,7 +116,6 @@ is visible even if it isn't selected"""
 
     def __init__(self, choices, parent=None, editable=True, **kwargs):
         ComboBoxDelegate.__init__(self, parent, choices, editable, **kwargs)
-        print 'choices by construction', choices(None)
 
     def setEditorData(self, editor, index):
         value = variant_to_pyobject(index.data(Qt.EditRole))
@@ -153,76 +152,7 @@ is visible even if it isn't selected"""
         check_option = QtGui.QStyleOptionMenuItem()
         #check_option.OptionType = QStyleOption.SO_ComboBox
 
-        #check_option.text = QString('choice')
-
-
-class ComboBoxDelegate(CustomDelegate):
-  
-  __metaclass__ = DocumentationMetaclass
-
-  editor = editors.ChoicesEditor
-  
-  def __init__(self, parent, choices, editable=True, **kwargs):
-    CustomDelegate.__init__(self, parent, editable=editable, **kwargs)
-    self.choices = choices
-
-              
-  def setEditorData(self, editor, index):
-    value = variant_to_pyobject(index.data(Qt.EditRole))
-    
-    def create_choices_getter(model, row):
-      
-      def choices_getter():
-        return list(self.choices(model._get_object(row)))
-      
-      return choices_getter
-    
-    editor.set_value(value)
-    post(create_choices_getter(index.model(),index.row()), editor.set_choices)
-
-  def paint(self, painter, option, index):
-    painter.save()
-    self.drawBackground(painter, option, index)
-    value = variant_to_pyobject(index.data(Qt.EditRole))
-    
-    background_color = QtGui.QColor(index.model().data(index, Qt.BackgroundRole))
-    
-    rect = option.rect
-    rect = QtCore.QRect(rect.left() + 3, rect.top() + 6, rect.width() - 5, rect.height())
-    
-    if(option.state & QtGui.QStyle.State_Selected):
-        painter.fillRect(option.rect, option.palette.highlight())
-        fontColor = QtGui.QColor()
-        if self.editable:         
-          Color = option.palette.highlightedText().color()
-          fontColor.setRgb(Color.red(), Color.green(), Color.blue())
-        else:          
-          fontColor.setRgb(130, 130, 130)
-    else:
-        if self.editable:
-          painter.fillRect(option.rect, background_color)
-          fontColor = QtGui.QColor()
-          fontColor.setRgb(0, 0, 0)
-        else:
-          painter.fillRect(option.rect, option.palette.window())
-          fontColor = QtGui.QColor()
-          fontColor.setRgb(130, 130, 130)
-          
-    
-    
-    painter.setPen(fontColor.toRgb())
-    rect = QtCore.QRect(option.rect.left()+2,
-                        option.rect.top(),
-                        option.rect.width()-4,
-                        option.rect.height())
-    painter.drawText(rect.x(),
-                     rect.y(),
-                     rect.width(),
-                     rect.height(),
-                     Qt.AlignVCenter | Qt.AlignLeft,
-                     unicode(value))
-    painter.restore()
-    
+        #check_option.text = QString('choice')    
     
 class ComboBoxEditorDelegate(ComboBoxDelegate):  
     
@@ -234,15 +164,13 @@ class ComboBoxEditorDelegate(ComboBoxDelegate):
     """
     def __init__(self, choices, parent=None, editable=True, **kwargs):
          ComboBoxDelegate.__init__(self, parent, choices, editable, **kwargs)
-         print 'choices by construction', choices(None)
          
     def setEditorData(self, editor, index):
         value = variant_to_pyobject(index.data(Qt.EditRole))
 
         def create_choices_getter(model, row):
           
-          def choices_getter():
-            print 'in set editor data', self.choices(None)
+          def choices_getter():
             return list(self.choices(None))
           
           return choices_getter
