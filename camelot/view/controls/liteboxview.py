c6e6995575843db5fc303fa570419753350830a7
victorn <victorn@8325946a-aa55-0410-8760-f0bc8c33efaa>
2010-01-15 15:03:12 +0000

    works with activation and deactivation now
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@1093 8325946a-aa55-0410-8760-f0bc8c33efaa

--- camelot/view/controls/liteboxview.py
+++ camelot/view/controls/liteboxview.py
@@ -1,13 +1,39 @@
-# description: lightbox effect a la javascipt done with pyqt
-# author: victor noagbodji
+#  ============================================================================
+#
+#  Copyright (C) 2007-2010 Conceptive Engineering bvba. All rights reserved.
+#  www.conceptive.be / project-camelot@conceptive.be
+#
+#  This file is part of the Camelot Library.
+#
+#  This file may be used under the terms of the GNU General Public
+#  License version 2.0 as published by the Free Software Foundation
+#  and appearing in the file LICENSE.GPL included in the packaging of
+#  this file.  Please review the following information to ensure GNU
+#  General Public Licensing requirements will be met:
+#  http://www.trolltech.com/products/qt/opensource.html
+#
+#  If you are unsure which license is appropriate for your use, please
+#  review the following information:
+#  http://www.trolltech.com/products/qt/licensing.html or contact
+#  project-camelot@conceptive.be.
+#
+#  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+#  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  For use of this library in commercial applications, please contact
+#  project-camelot@conceptive.be
+#
+#  ============================================================================
 
 import sys
 from PyQt4.QtGui import (
-    QApplication,
+    QApplication, QWidget,
     QColor, QPixmap, QPushButton,
-    QGraphicsView, QGraphicsScene
+    QGraphicsView, QGraphicsScene,
 )
-from PyQt4.QtCore import Qt, QPoint
+from PyQt4.QtCore import (
+    Qt, QPoint, QEvent
+)
 
 
 def get_desktop():
@@ -31,14 +57,13 @@ def fit_to_screen(pixmap):
 
 class LiteBoxView(QGraphicsView):
 
-    def __init__(self, path, parent=None):
+    ALPHABLACK = QColor(0, 0, 0, 192)
+
+    def __init__(self, parent=None):
         super(LiteBoxView, self).__init__(parent)
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-
         self.scene = QGraphicsScene()
-        self.path = path
-        self.pixitem = self.scene.addPixmap(fit_to_screen(QPixmap(self.path)))
         self.setScene(self.scene)
 
     def mousePressEvent(self, event):
@@ -46,12 +71,36 @@ class LiteBoxView(QGraphicsView):
 
     def drawBackground(self, painter, rect):
         painter.drawPixmap(self.mapToScene(0, 0), get_desktop_pixmap())
-        painter.setBrush(QColor(0, 0, 0, 127))
+        painter.setBrush(LiteBoxView.ALPHABLACK)
         painter.drawRect(rect)
+
+    def activateOn(self, widget):
+        widget.installEventFilter(self)
+
+    def deactivateFrom(self, widget):
+        widget.removeEventFilter(self)
+
+    def eventFilter(self, object, event):
+        if not object.isWidgetType():
+            return False
+
+        if event.type() != QEvent.MouseButtonDblClick:
+            return False
+
+        if event.modifiers() != Qt.NoModifier:
+            return False
+
+        if event.buttons() == Qt.LeftButton:
+            pixmap = QPixmap.grabWidget(object)
+            self.scene.addPixmap(fit_to_screen(pixmap))
+            self.showFullScreen()
+            return True
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    view = LiteBoxView('anime.jpg')
-    view.showFullScreen()
+    w = QWidget()
+    v = LiteBoxView()
+    v.activateOn(w)
+    w.show()
     sys.exit(app.exec_())
