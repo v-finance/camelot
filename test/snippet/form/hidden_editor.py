c99ea9cfab8bb68f3a9f34413cbd4d9af89460a2
erikj <erikj@8325946a-aa55-0410-8760-f0bc8c33efaa>
2010-02-01 22:07:02 +0000

    add a hidden widget snippet
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@1142 8325946a-aa55-0410-8760-f0bc8c33efaa

---
+++ test/snippet/form/hidden_editor.py
@@ -0,0 +1,48 @@
+from PyQt4 import QtCore
+
+from camelot.view import forms
+from camelot.view.controls import delegates
+from camelot.admin.object_admin import ObjectAdmin
+from camelot.admin.application_admin import ApplicationAdmin
+
+class FormWithHiddenWidget(forms.Form):
+    
+    def render(self, widgets, *args, **kwargs):
+        form_widget = super(FormWithHiddenWidget, self).render(widgets, *args, **kwargs)
+        
+        _show_more_label, show_more_editor = widgets['show_more']
+        hidden_label, hidden_editor        = widgets['details']
+        
+        def state_change(i):
+            hidden_label.setVisible(i)
+            hidden_editor.setVisible(i)
+            
+        form_widget.connect(show_more_editor, QtCore.SIGNAL('stateChanged(int)'), state_change)
+        hidden_label.hide()
+        hidden_editor.hide()
+        return form_widget
+        
+class ExampleObject(object):
+    
+    def __init__(self):
+        self.show_more = False
+        self.details = u''
+        
+    class Admin(ObjectAdmin):
+        form_display = FormWithHiddenWidget(['show_more', 'details'])
+        field_attributes = {'show_more':{'delegate':delegates.BoolDelegate, 'editable':True},
+                            'details':{'delegate':delegates.RichTextDelegate, 'editable':True}}
+        
+if __name__ == "__main__":
+    from camelot.view.main import main
+    from PyQt4 import QtGui
+    app_admin = ApplicationAdmin()
+    
+    def show_form():
+        dialog = QtGui.QDialog()
+        example_admin = ExampleObject.Admin(app_admin, ExampleObject)
+        form = example_admin.create_new_view(parent=dialog)
+        form.show()
+        dialog.exec_()
+        
+    main(app_admin, initialization=show_form)
