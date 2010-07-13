3ad69d9652197baee6540faa42a5a4bda78935a9
jeroen <jeroen@8325946a-aa55-0410-8760-f0bc8c33efaa>
2010-07-13 13:35:44 +0000

    default backup location added (patch: Ioan Calin Borcoman)
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@1319 8325946a-aa55-0410-8760-f0bc8c33efaa

--- camelot/empty_project/settings.py
+++ camelot/empty_project/settings.py
@@ -8,6 +8,12 @@ CAMELOT_ATTACHMENTS = ''
 # to function correctly
 CAMELOT_MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media')
 
+# backup root is the directory where the default backups are stored
+CAMELOT_BACKUP_ROOT = os.path.join(os.path.dirname(__file__), 'backup')
+
+# template used to create and find default backups
+CAMELOT_BACKUP_FILENAME_TEMPLATE = 'default-backup-%(text)s.sqlite'
+
 REPOSITORY = 'repository'
 ENGINE = lambda:'sqlite:///model-data.sqlite'
 
