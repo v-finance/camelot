52eda52b9a422ba2ae0fadadbae2a8bd77d704da
erikj <erikj@8325946a-aa55-0410-8760-f0bc8c33efaa>
2010-09-12 17:41:46 +0000

    test registering models and views
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@1504 8325946a-aa55-0410-8760-f0bc8c33efaa

--- test/test_qt_bindings.py
+++ test/test_qt_bindings.py
@@ -2,35 +2,50 @@
 """
 
 import unittest
-import logging
-logger = logging.getLogger( 'test_qt_bindings' )
 
-from PyQt4 import QtGui
-#from PySide import QtGui
+from PyQt4 import QtGui, QtCore
 
 class ReferenceHoldingBox(QtGui.QGroupBox):
-    """A group box implicitely holding references to the table
+    """A group box holding references to the table
     view and the table model"""
 
-    def __init__(self, model_name_getter, table_name_getter):
+    def __init__(self, model, table):
         QtGui.QGroupBox.__init__(self)
-        self.model_name_getter = model_name_getter
-        self.table_name_getter = table_name_getter
+        self.model = model
+        self.table = table
                  
 class TableView( QtGui.QWidget  ):
     """A widget containg both a table and a groupbox that
     holds a reference to both the table and the model of the
     table"""
 
-    def __init__( self, parent = None ):
-        super(TableView, self).__init__( parent )
+    def __init__( self, table_model ):
+        super(TableView, self).__init__()
         widget_layout = QtGui.QVBoxLayout()
-        self.table = QtGui.QTableView( self )
-        table_model = self.table_model()
-        self.table.setModel( table_model )
-        widget_layout.addWidget( self.table )        
-        widget_layout.addWidget( ReferenceHoldingBox( table_model.objectName, self.objectName ) )
+        table_model.setParent( self )
+        table = QtGui.QTableView( self )
+        table.setModel( table_model )
+        widget_layout.addWidget( table )
+        widget_layout.addWidget( ReferenceHoldingBox( table_model, self ) )
         self.setLayout( widget_layout )
+
+class ModelViewRegister(QtCore.QObject):
+    
+    def __init__(self):
+        super(ModelViewRegister, self).__init__()
+        self.max_key = 0
+        self.model_by_view = dict()
+        
+    def register_model_view(self, model, view):
+        self.max_key += 1
+        view.destroyed.connect( self._registered_object_destroyed )
+        self.model_by_view[self.max_key] = model
+        view.setProperty( 'registered_key', self.max_key )
+        
+    @QtCore.pyqtSlot(QtCore.QObject)
+    def _registered_object_destroyed(self, qobject):
+        key, _success = qobject.property('registered_key').toLongLong()
+        del self.model_by_view[key]
 
 class TableViewCases(unittest.TestCase):
     """Tests related to table views"""
@@ -38,17 +53,23 @@ class TableViewCases(unittest.TestCase):
     def setUp(self):
         self.application = QtGui.QApplication([])
 
-    def test_select_view_garbage_collection(self):
-        """Create a select view and force its garbage collection"""
+    def test_table_view_garbage_collection(self):
+        """Create a table view and force its garbage collection, while
+        a common reference exists to both the table view and its model.
+        
+        when doing so without registering the model and the view to the
+        ModelViewRegister, this will segfault.
+        """            
+        register = ModelViewRegister()
+        
         import gc
-        for i in range(100):
+        for i in range(5):
             print i
             
             class TableModelSubclass(QtGui.QStringListModel):
                 pass
     
-            class TableViewSubclass(TableView):
-                table_model = TableModelSubclass
-    
-            widget = TableViewSubclass()
+            model = TableModelSubclass()
+            widget = TableView( model )
+            register.register_model_view(model, widget)
             gc.collect()
