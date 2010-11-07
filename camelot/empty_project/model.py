f8449b4946f99b38a510e7da9b9c5ce558d677e1
erikj <erikj@8325946a-aa55-0410-8760-f0bc8c33efaa>
2010-11-07 23:11:37 +0000

    remove cr at end of line
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@1628 8325946a-aa55-0410-8760-f0bc8c33efaa

--- camelot/empty_project/model.py
+++ camelot/empty_project/model.py
@@ -1,6 +1,9 @@
-import camelot.types
-from camelot.model import Entity, Field, ManyToOne, OneToMany, Unicode, Date, Integer, using_options
-from camelot.admin.entity_admin import EntityAdmin
+#
+# Uncomment these import lines as needed
+#
+#import camelot.types
+#from camelot.model import Entity, Field, ManyToOne, OneToMany, Unicode, Date, Integer, using_options
+#from camelot.admin.entity_admin import EntityAdmin
 
 # begin meta data setup
 
@@ -9,3 +12,4 @@ from camelot.model import metadata
 __metadata__ = metadata
 
 # end meta data setup
+
