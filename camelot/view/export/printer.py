ed73f93406bc31304e05fc508d53b02722345aff
victorn <victorn@8325946a-aa55-0410-8760-f0bc8c33efaa>
2010-12-23 10:59:24 +0000

    '
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@1736 8325946a-aa55-0410-8760-f0bc8c33efaa

--- camelot/view/export/printer.py
+++ camelot/view/export/printer.py
@@ -22,37 +22,30 @@
 #
 #  ============================================================================
 import logging
-logger = logging.getLogger( 'camelot.view.export.printer' )
+logger = logging.getLogger('camelot.view.export.printer')
 
-from PyQt4 import QtCore
-from PyQt4.QtCore import QCoreApplication
-
-from PyQt4.QtGui import QPrinter
-from PyQt4.QtGui import QTextDocument
-from PyQt4.QtGui import QPrintPreviewDialog
-
+from PyQt4 import QtGui, QtCore
 from camelot.view.model_thread import gui_function
 
+
 @gui_function
-def open_html_in_print_preview_from_gui_thread(html,
-    html_document=QTextDocument, page_size=None, page_orientation=None):
+def open_html_in_print_preview_from_gui_thread(html, html_document=QtGui.QTextDocument):
 
-    printer = QPrinter()
-    printer.setPageSize(page_size or QPrinter.A4)
-    printer.setOrientation(page_orientation or QPrinter.Portrait)
+    printer = QtGui.QPrinter()
+    printer.setPageSize( QtGui.QPrinter.A4 )
+# TODO: make landscape optional
+#  printer.setOrientation(QtGui.QPrinter.Landscape)
     # TODO: maximize button
-    dialog = QPrintPreviewDialog(printer)
+    dialog = QtGui.QPrintPreviewDialog( printer )
 
-    @QtCore.pyqtSlot(QPrinter)
-    def render(printer):
+    @QtCore.pyqtSlot( QtGui.QPrinter )
+    def render( printer ):
         doc = html_document()
-        doc.setHtml(html)
-        doc.print_(printer)
+        doc.setHtml( html )
+        doc.print_( printer )
 
-    dialog.paintRequested.connect(render)
+    dialog.paintRequested.connect( render )
     # show maximized seems to trigger a bug in qt which scrolls the page down
     #dialog.showMaximized()
-    desktop = QCoreApplication.instance().desktop()
-    # use the size of the desktop instead to set the dialog size
-    dialog.resize(desktop.width() * 0.75, desktop.height() * 0.75)
     dialog.exec_()
+
