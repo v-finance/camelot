9ac9a7cc94838a6a8a86d647fd0c03b398f54b59
erikj <erikj@8325946a-aa55-0410-8760-f0bc8c33efaa>
2011-04-06 16:04:08 +0000

    add notes to tasks
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@1972 8325946a-aa55-0410-8760-f0bc8c33efaa

--- camelot/model/task.py
+++ camelot/model/task.py
@@ -28,13 +28,17 @@ These classes provide basic task tracking with configurable statuses,
 categories and roles.  They are presented to the user as "Todo's"
 """
 
-from elixir import Entity, using_options, Field, ManyToMany
+from elixir import Entity, using_options, Field, ManyToMany, OneToMany, ManyToOne
 import sqlalchemy.types
 
 from camelot.core.utils import ugettext_lazy as _
 from camelot.model import metadata
+from camelot.model.authentication import getCurrentAuthentication
 from camelot.admin.entity_admin import EntityAdmin
 from camelot.core.document import documented_entity
+from camelot.view import forms
+from camelot.view.controls import delegates
+import camelot.types
 
 import datetime
 
@@ -45,17 +49,45 @@ class Task( Entity ):
     creation_date    = Field( sqlalchemy.types.Date, required=True, default=datetime.date.today )
     due_date         = Field( sqlalchemy.types.Date, required=False, default=None )
     description      = Field( sqlalchemy.types.Unicode(255), required=True )
+    notes            = OneToMany( 'TaskNote', cascade='all, delete, delete-orphan' )
     categories = ManyToMany( 'PartyCategory',
                              tablename='party_category_task', 
                              remote_colname='party_category_id',
                              local_colname='task_id')
     
+    def _get_first_note(self):
+        if self.notes:
+            return self.notes[0].note
+
+    def _set_first_note(self, note):
+        if note and self.id:
+            if self.notes:
+                self.notes[0].note = note
+            else:
+                self.notes.append( TaskNote( note=note, created_by=getCurrentAuthentication() ) )
+ 
+    note = property( _get_first_note, _set_first_note )
+    
     class Admin( EntityAdmin ):
         verbose_name = _('Todo')
         list_display = ['creation_date', 'description', 'due_date']
         list_filter  = ['categories.name']
-        form_display = ['description', 'creation_date', 'due_date', 
-                        'categories']
+        form_display = forms.Form( ['description', 'creation_date', 'due_date', 
+                        'note', 'categories'] )
+        field_attributes = {'note':{'delegate':delegates.RichTextDelegate,
+                                    'editable':lambda self:self.id}}
 
 Task = documented_entity()( Task )
+
+class TaskNote( Entity ):
+    using_options(tablename='task_note', order_by=['-created_at'] )
+    of = ManyToOne('Task', required=True, onupdate='cascade', ondelete='cascade')
+    created_at = Field( sqlalchemy.types.Date, required=True, default=datetime.date.today )
+    created_by = ManyToOne('AuthenticationMechanism', required=True )
+    note = Field( camelot.types.RichText() )
+  
+    class Admin( EntityAdmin ):
+        verbose_name = _('Note')
+        list_display = ['created_at', 'created_by']
+        form_display = list_display + ['note']
 
