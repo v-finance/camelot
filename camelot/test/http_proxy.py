50a1cbf6341a188a2820d757080b80cd132a64c5
victorn <victorn@8325946a-aa55-0410-8760-f0bc8c33efaa>
2011-04-22 09:34:48 +0000

    custom qt based http proxy added
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@1986 8325946a-aa55-0410-8760-f0bc8c33efaa

---
+++ camelot/test/http_proxy.py
@@ -0,0 +1,199 @@
+#  ============================================================================
+#
+#  Copyright (C) 2007-2011 Conceptive Engineering bvba. All rights reserved.
+#  www.conceptive.be / project-camelot@conceptive.be
+#
+#  This file is part of the Camelot Library.
+#
+#  This file may be used under the terms of the GNU General Public
+#  License version 2.0 as published by the Free Software Foundation
+#  and appearing in the file license.txt included in the packaging of
+#  this file.  Please review this information to ensure GNU
+#  General Public Licensing requirements will be met.
+#
+#  If you are unsure which license is appropriate for your use, please
+#  visit www.python-camelot.com or contact project-camelot@conceptive.be
+#
+#  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+#  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  For use of this library in commercial applications, please contact
+#  project-camelot@conceptive.be
+#
+#  ============================================================================
+
+
+import sys
+import base64
+
+from PyQt4.QtCore import QUrl
+from PyQt4.QtCore import QString
+from PyQt4.QtCore import QObject
+from PyQt4.QtCore import pyqtSlot
+from PyQt4.QtCore import QCoreApplication
+from PyQt4.QtNetwork import QTcpSocket
+from PyQt4.QtNetwork import QTcpServer
+from PyQt4.QtNetwork import QHostAddress
+from PyQt4.QtNetwork import QHttpRequestHeader
+
+from PyQt4.QtNetwork import QNetworkProxy
+from PyQt4.QtNetwork import QNetworkRequest
+from PyQt4.QtNetwork import QNetworkAccessManager
+
+
+# The following http proxy is a modified version of the one found here
+# http://gitorious.org/ofi-labs/x2/blobs/master/network/webproxy/webproxy.cpp
+# It adds basic http authentication, a stopping slot, and debugging
+class HTTPProxy(QObject):
+
+    debug = True
+    log = sys.stderr
+    username = 'username'
+    password = 'password'
+    stopServing = pyqtSlot()
+
+    def __init__(self, parent=None):
+        super(HTTPProxy, self).__init__(parent)
+        self.proxy_server = QTcpServer(self)
+        self.proxy_server.listen(QHostAddress.Any, 8000)
+        self.proxy_server.newConnection.connect(self.manage_request)
+
+        if self.debug:
+            self.log.write('Proxy server running on 0.0.0.0 port %s\n' %
+                self.port())
+
+    def port(self):
+        return self.proxy_server.serverPort()
+
+    def addr(self):
+        return self.proxy_server.serverAddress().toString()
+
+    def stopServing(self):
+        if self.debug:
+            self.log.write('Service is stopping...\n')
+        self.proxy_server.close()
+
+    def manage_request(self):
+        proxy_server = self.sender()
+        # Qt docs says that the caller of nextPendingConnection()
+        # is the parent of the socket
+        socket = proxy_server.nextPendingConnection()
+        socket.readyRead.connect(self.process_request)
+        socket.disconnected.connect(socket.deleteLater)
+
+    def authorize_request(self, request_data):
+        header = QHttpRequestHeader(QString(request_data))
+        auth = header.value('Proxy-Authorization')
+        if not auth:
+            return False
+
+        challenge = base64.b64encode(self.username+':'+self.password)
+        return challenge == str(auth).split()[1]
+
+    def process_request(self):
+        socket = self.sender()
+        request_data = socket.readAll()
+
+        if not self.authorize_request(request_data):
+            socket.write('HTTP/1.1 407 Proxy Authentication Required\r\n')
+            if self.debug:
+                self.log.write('407 Proxy Authentication Required\n')
+            socket.write('Proxy-Authenticate: Basic realm="test"\r\n')
+            socket.write('\r\n')
+            socket.disconnectFromHost()
+            return
+        else:
+            # remove Proxy-Authorization header
+            start = request_data.indexOf('Proxy-Authorization:')
+            end = request_data.lastIndexOf('\r\n')
+            request_data.remove(start, end)
+
+        pos = request_data.indexOf('\r\n')
+        request_line = request_data.left(pos)
+        request_data.remove(0, pos + 2)
+
+        entries = request_line.split(' ')
+        method = entries[0]
+        address = entries[1]
+        version = entries[2]
+
+        url = QUrl.fromEncoded(address)
+        if not url.isValid():
+            if self.debug:
+                self.log.write('Invalid URL: %s\n', url)
+            socket.disconnectFromHost()
+            return
+
+        host = url.host()
+        port = 80 if (url.port() < 0) else url.port()
+        req = url.encodedPath()
+        if url.hasQuery():
+            req.append('?').append(url.encodedQuery())
+        request_line = method + ' ' + req + ' ' + version + '\r\n'
+        request_data.prepend(request_line)
+
+        if self.debug:
+            self.log.write(method + ' ' + address + ' ' + version + '\n')
+
+        key = host + ':' + QString.number(port)
+        proxy_socket = socket.findChild(QTcpSocket, key)
+        if proxy_socket:
+            proxy_socket.setObjectName(key)
+            proxy_socket.setProperty('url', url)
+            proxy_socket.setProperty('request_data', request_data)
+            proxy_socket.write(request_data)
+        else:
+            proxy_socket = QTcpSocket(socket)
+            proxy_socket.setObjectName(key)
+            proxy_socket.setProperty('url', url)
+            proxy_socket.setProperty('request_data', request_data)
+            proxy_socket.connected.connect(self.send_request)
+            proxy_socket.readyRead.connect(self.transfer_data)
+            proxy_socket.disconnected.connect(self.close_connection)
+            proxy_socket.error.connect(self.close_connection)
+            proxy_socket.connectToHost(host, port)
+
+    def send_request(self):
+        proxy_socket = self.sender()
+        request_data = proxy_socket.property('request_data').toByteArray()
+        proxy_socket.write(request_data)
+
+    def transfer_data(self):
+        proxy_socket = self.sender()
+        socket = proxy_socket.parent()
+        socket.write(proxy_socket.readAll())
+
+    def close_connection(self):
+        proxy_socket = self.sender()
+        if proxy_socket:
+            socket = proxy_socket.parent()
+            if isinstance(socket, QTcpSocket) and socket:
+                socket.disconnectFromHost()
+            if proxy_socket.error() != QTcpSocket.RemoteHostClosedError:
+                url = proxy_socket.property('url').toUrl()
+                error_string = proxy_socket.errorString()
+
+                if self.debug:
+                    self.log.write('Error for %s %s' % (url, error_string))
+
+            proxy_socket.deleteLater()
+
+
+if __name__ == '__main__':
+    app = QCoreApplication(sys.argv)
+
+    server = HTTPProxy()
+    manager = QNetworkAccessManager()
+    manager.finished.connect(server.stopServing)
+
+    proxy = QNetworkProxy()
+    proxy.setType(QNetworkProxy.HttpProxy)
+    proxy.setHostName('127.0.0.1')
+    proxy.setPort(server.port())
+    proxy.setUser(server.username)
+    proxy.setPassword(server.password)
+
+    manager.setProxy(proxy)
+    manager.get(QNetworkRequest(QUrl('http://aws.amazon.com/')))
+
+    sys.exit(app.exec_())
---
+++ camelot/test/test_proxy.py
@@ -0,0 +1,129 @@
+#  ============================================================================
+#
+#  Copyright (C) 2007-2011 Conceptive Engineering bvba. All rights reserved.
+#  www.conceptive.be / project-camelot@conceptive.be
+#
+#  This file is part of the Camelot Library.
+#
+#  This file may be used under the terms of the GNU General Public
+#  License version 2.0 as published by the Free Software Foundation
+#  and appearing in the file license.txt included in the packaging of
+#  this file.  Please review this information to ensure GNU
+#  General Public Licensing requirements will be met.
+#
+#  If you are unsure which license is appropriate for your use, please
+#  visit www.python-camelot.com or contact project-camelot@conceptive.be
+#
+#  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+#  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  For use of this library in commercial applications, please contact
+#  project-camelot@conceptive.be
+#
+#  ============================================================================
+
+import sys
+import unittest
+import subprocess
+from subprocess import PIPE
+
+from PyQt4 import QtGui
+from PyQt4 import QtCore
+from PyQt4 import QtNetwork
+from PyQt4.QtCore import QTimer
+from PyQt4.QtCore import QEventLoop
+
+from camelot.test.http_proxy import HTTPProxy
+from camelot.core.dbprofiles import get_network_proxy
+
+
+_application_ = None
+if not QtGui.QApplication.instance():
+    _application_ = QtGui.QApplication([a for a in sys.argv if a])
+
+
+def setup_a_win32_http_system_proxy(hostname, port):
+    # XXX:
+    # ProxyHTTPServer is not setting up a system proxy with regards to
+    # Windows. PyQt will go through the registry first and look for a
+    # system proxy setting, then through Internet Explorer's settings.
+    # Not sure how Internet Explorer's settings can be changed
+    # programmatically so we will simulate a system proxy with proxcfg
+    # which should be in PATH
+    # reference page:
+    # http://msdn.microsoft.com/en-us/library/aa384069%28v=vs.85%29.aspx
+    http_proxy_value = 'http=http://%s:%s' % (hostname, port)
+    command = ['proxycfg', '-p', http_proxy_value, '"<local>"']
+    subprocess.call(command, shell=True, stdin=PIPE, stdout=PIPE,
+        stderr=PIPE, universal_newlines=True)
+
+
+def clear_win_http_settings():
+    # XXX:
+    # setting a system proxy in Windows writes to the registry and that
+    # key needs to be deleted as said on the reference page, but a user
+    # might have set a "real" proxy using the same WinHTTPSettings ...
+    import win32api
+    import win32con
+    opened_key = win32api.RegOpenKeyEx(win32con.HKEY_LOCAL_MACHINE,
+        'SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings'\
+        '\Connections', 0, win32con.KEY_ALL_ACCESS)
+    win32api.RegDeleteValue(opened_key, 'WinHttpSettings')
+
+    ERROR_SUCCESS = 0
+    error_code = win32api.GetLastError()
+    if error_code != ERROR_SUCCESS:
+        print 'win32 error code %d', error_code, \
+            win32api.FormatMessage(error_code)
+
+
+class ProxyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.server = HTTPProxy()
+        setup_a_win32_http_system_proxy('127.0.0.1', 8000)
+
+    def tearDown(self):
+        self.server.stopServing()
+        clear_win_http_settings()
+
+    def test_proxy(self):
+        proxy = get_network_proxy()
+        self.assertTrue(isinstance(proxy, QtNetwork.QNetworkProxy))
+        self.assertEquals(str(proxy.hostName()), '127.0.0.1')
+        self.assertEquals(proxy.port(), 8000)
+
+        #manager = QtNetwork.QNetworkAccessManager()
+        #manager.setProxy(proxy)
+
+        #event_loop = QEventLoop()
+        #request_timer = QTimer()
+        #request_timer.timeout.connect(event_loop.exit)
+        #manager.finished.connect(event_loop.exit)
+
+        #request = QtNetwork.QNetworkRequest()
+        #request.setUrl(QtCore.QUrl('http://aws.amazon.com/'))
+        #reply = manager.get(request)
+        #self.server.handler_request()
+        #reply.readyRead.connect(self.server.handle_request)
+        #request_timer.start(5*1000)
+        #event_loop.exec_(flags=QEventLoop.ExcludeUserInputEvents)
+
+        #if reply.isFinished():
+        #    parts = self.server_log.getvalue().split()
+        #    hostname = parts[0]
+        #    url = parts[6]
+        #    response_code = parts[8]
+        #    self.assertTrue(hostname in ['localhost', '127.0.0.1'])
+        #    self.assertEquals(url, 'http://aws.amazon.com/')
+        #    self.assertEquals(response_code, '200')
+        #else:
+        #    if reply.isRunning():
+        #        self.failUnless(False, msg='The request has timed out.')
+        #    else:
+        #        self.failUnless(False, msg='Something else happened; the '
+        #            'request is not running.')
+
+
+if __name__ == '__main__':
+    unittest.main()
