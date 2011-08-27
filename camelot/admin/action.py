2edd61c9391d9cd8089dd562f6e7059d8a01d509
erikj <erikj@8325946a-aa55-0410-8760-f0bc8c33efaa>
2011-08-27 08:07:01 +0000

    add first action step implementation
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@2197 8325946a-aa55-0410-8760-f0bc8c33efaa

---
+++ camelot/admin/action.py
@@ -0,0 +1,153 @@
+#  ============================================================================
+#
+#  Copyright (C) 2007-2011 Conceptive Engineering bvba. All rights reserved.
+#  www.conceptive.be / project-camelot@conceptive.be
+#
+#  This file is part of the Camelot Library.
+#
+#  This file may be used under the terms of the GNU General Public
+#  License version 2.0 as published by the Free Software Foundation
+#  and appearing in the file license.txt included in the packaging of
+#  this file.  Please review this information to ensure GNU
+#  General Public Licensing requirements will be met.
+#
+#  If you are unsure which license is appropriate for your use, please
+#  visit www.python-camelot.com or contact project-camelot@conceptive.be
+#
+#  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+#  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  For use of this library in commercial applications, please contact
+#  project-camelot@conceptive.be
+#
+#  ============================================================================
+
+"""
+This is part of a test implementation of the new actions draft, it is not
+intended for production use
+"""
+
+import logging
+
+from PyQt4 import QtCore
+
+from camelot.view.model_thread import post
+from camelot.core.exception import GuiException
+
+LOGGER = logging.getLogger( 'camelot.admin.action' )
+
+class GuiContext( object ):
+    
+    def __init__( self, admin = None, workspace = None ):
+        """The context of an action available in the *GUI thread*
+        during the execution of the action.
+        :param admin: the Admin class to use during the execution of the action,
+           to be used to retrieve Admin classes for classes when needed.
+           defaults to the unique ApplicationAdmin.
+        """
+        self._admin = admin
+        self._workspace = workspace
+        
+    def get_admin( self ):
+        """
+        :return: the admin class that can be used to retrieve related admin
+           classes
+        """
+        from camelot.admin.application_admin import get_application_admin
+        return self._admin or get_application_admin()
+    
+    def get_workspace( self ):
+        return self._workspace
+    
+class ActionRunner( QtCore.QEventLoop ):
+    """Helper class for handling the signals and slots when an action
+    is running.
+    """
+    
+    def __init__( self, generator_function, gui_context ):
+        """
+        :param generator_function: function to be called in the model thread,
+            that will return a generator
+        :param gui_context: the GUI context of the generator
+        """
+        super( ActionRunner, self ).__init__()
+        self._generator = None
+        self._gui_context = gui_context
+        post( generator_function, self.generator, self.exception )
+        print 'posted'
+        
+    @QtCore.pyqtSlot( object )
+    def exception( self, exception_info ):
+        print 'exception'
+        self.exit()
+        
+    @QtCore.pyqtSlot( object )
+    def generator( self, generator ):
+        self._generator = generator
+        print 'generator ready'
+        post( self._generator.next, self.next, self.exception )
+        
+    @QtCore.pyqtSlot( object )
+    def next( self, yielded ):
+        """Handle the result of the next call of the generator
+        
+        :param yielded: the object that was yielded by the generator in the
+            *model thread*
+        """
+        print 'next'
+        if isinstance( yielded, (ActionStep,) ):
+            try:
+                to_send = yielded.gui_run( self._gui_context )
+                post( self._generator.send, 
+                      self.next, 
+                      self.exception, 
+                      args = (to_send,) )
+            except Exception, exc:
+                LOGGER.error( 'gui exception while executing action', 
+                              exc_info=exc)
+                #
+                # In case of an exception in the GUI thread, propagate an
+                # exception to make sure the generator ends.  Don't propagate
+                # the very same exception, because no references from the GUI
+                # should be past to the model.
+                #
+                post( self._generator.throw,
+                      self.next,
+                      self.exception,
+                      args = ( GuiException(), ) )
+        self.exit()
+    
+class ActionStep( object ):
+
+    def gui_run( self, gui_context ):
+        """This method is called in the *GUI thread* upon execution of the
+        action step.  The return value of this method is the result of the
+        :keyword:`yield` statement in the *model thread*.
+        
+        The default behavior of this method is to call the model_run generator
+        in the *model thread* until it is finished.
+        
+        :param gui_context:  An object of type 
+            :class:`camelot.admin.action.GuiContext`, which is the context 
+            of this action available in the *GUI thread*.  What is in the 
+            context depends on how the action was called.
+        """
+        runner = ActionRunner( self.model_run, gui_context )
+        runner.exec_()
+        
+        print 'gui_run'
+        generator = self.model_run( None )
+        print generator
+        
+    def model_run( self, model_context = None ):
+        """A generator that yields :class:`camelot.admin.action.ActionStep`
+        objects.  This generator can be called in the *model thread*.
+        
+        :param context:  An object of type
+            :class:`camelot.admin.action.ModelContext`, which is context 
+            of this action available in the model_thread.  What is in the 
+            context depends on how the action was called.
+        """
+        print 1
+        yield
+        print 2
