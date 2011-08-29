9dc571465e7e30468656473b9857bde510e37882
erikj <erikj@8325946a-aa55-0410-8760-f0bc8c33efaa>
2011-08-29 22:55:29 +0000

    begin test cases for new actions
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@2205 8325946a-aa55-0410-8760-f0bc8c33efaa

---
+++ test/test_action.py
@@ -0,0 +1,22 @@
+from camelot.test import ModelThreadTestCase
+
+from test_view import static_images_path
+
+class ActionStepsCase(ModelThreadTestCase):
+    """Test the various steps that can be executed during an
+    action.
+    """
+
+    images_path = static_images_path
+    
+    def setUp(self):
+        from camelot.admin.application_admin import ApplicationAdmin
+        ModelThreadTestCase.setUp(self)
+        self.app_admin = ApplicationAdmin()
+        
+    def test_change_object( self ):
+        from camelot.bin.meta import NewProjectOptions
+        from camelot.view.action_steps.change_object import ChangeObjectDialog
+        admin = NewProjectOptions.Admin( self.app_admin, NewProjectOptions )
+        dialog = ChangeObjectDialog( NewProjectOptions(), admin )
+        self.grab_widget( dialog )
