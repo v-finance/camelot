986a6812b2c3407bf0576b39f69ad0fb8019997b
Erik Janssens <erik.janssens@vortex-financials.be>
2011-12-11 23:16:21 +0100

    triger related actions status update when field attributes change

--- camelot/admin/object_admin.py
+++ camelot/admin/object_admin.py
@@ -457,9 +457,6 @@ be specified using the verbose_name attribute.
                         logger.error(u'error in field_attribute function of %s'%name, exc_info=exc)
                     finally:
                         dynamic_field_attributes[name] = return_value
-            #
-            # set editable to false in case of a relational field
-            #
             yield dynamic_field_attributes
 
     def get_field_attributes(self, field_name):
