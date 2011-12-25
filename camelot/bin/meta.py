3a37c277e9415427664b3be82db0fd3cb6116819
Erik Janssens <erik.janssens@vortex-financials.be>
2011-12-25 23:43:29 +0100

    add default unit tests to new project

--- camelot/bin/meta.py
+++ camelot/bin/meta.py
@@ -138,6 +138,26 @@ class MyApplicationAdmin(ApplicationAdmin):
     ('__init__.py', ''),
     
     ('{{options.module}}/__init__.py', ''),
+
+    ('{{options.module}}/test.py', '''
+#
+# Default unittests for a camelot application.  These unittests will create
+# screenshots of all the views in the application.  Run them with this command :
+#
+# python -m nose.core -v -s {{options.module}}/test.py
+#
+
+import os
+
+from camelot.test import EntityViewsTest
+
+# screenshots will be put in this directory
+static_images_path = os.path.join( os.path.dirname( __file__ ), 'images' )
+
+class MyApplicationViewsTest( EntityViewsTest ):
+
+    images_path = static_images_path
+    '''),
     
     ('main.py', '''
 import logging
