c1ad0b5616cf83b00b5b7f42a66372ea14a6b3d9
erikj <erikj@8325946a-aa55-0410-8760-f0bc8c33efaa>
2012-01-10 08:19:11 +0000

    search in different directories for translation files
    git-svn-id: http://projects.conceptive.be/camelot/svn/trunk@2443 8325946a-aa55-0410-8760-f0bc8c33efaa

--- test/test_i18n.py
+++ test/test_i18n.py
@@ -1,11 +1,23 @@
 from camelot.test import ModelThreadTestCase
 
+from PyQt4 import QtCore
+
 class I18NTest(ModelThreadTestCase):
 
-    def test_ugettext(self):
+    def test_ugettext( self ):
         from snippet.i18n.specify_translation_string import message
-        self.assertTrue(message)
+        self.assertTrue( message )
       
-    def test_ugettext_lazy(self):
+    def test_ugettext_lazy( self ):
         from snippet.i18n.specify_lazy_translation_string import message
-        self.assertTrue(message)
+        self.assertTrue( message )
+        
+    def test_load_translators( self ):
+        from camelot.admin.application_admin import ApplicationAdmin
+        app_admin = ApplicationAdmin()
+        QtCore.QLocale.setDefault( QtCore.QLocale( 'pt_BR' ) )
+        self.assertEqual( len( app_admin.get_translator() ), 2 )
+        QtCore.QLocale.setDefault( QtCore.QLocale( 'nl_BE' ) )
+        self.assertEqual( len( app_admin.get_translator() ), 1 )        
+        QtCore.QLocale.setDefault( QtCore.QLocale( 'pt' ) )
+        self.assertEqual( len( app_admin.get_translator() ), 1 )
