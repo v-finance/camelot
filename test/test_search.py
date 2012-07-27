9545ea6c586596e326c129dbdc02f6cfb6a236d7
Erik Janssens <erik.janssens@vortex-financials.be>
2012-07-27 15:23:23 +0200

    search for all String types

--- test/test_search.py
+++ test/test_search.py
@@ -31,6 +31,11 @@ class T( Entity ):
     """An entity with for each column type a column"""
     for (i,name), definition in types_to_test.items():
         has_field( name, definition )
+        
+class TAdmin( object ):
+    search_all_fields = True
+    list_search = []
+    entity = T
     
 class SearchCase( unittest.TestCase ):
     """Test the creation of search queries"""
@@ -40,25 +45,51 @@ class SearchCase( unittest.TestCase ):
         metadata.create_all()
         self.session = Session()
         #
-        # insert data in each column of T, that can be searched for
+        # insert the value of i in each column of T, that can be searched for
         #
         for (i,name), definition in types_to_test.items():
-            if issubclass( definition, sqlalchemy.types.DateTime ):
-                value = datetime.datetime( year = 2000, month = 1, day = 1, hour = 1, minute = i )       
-            elif issubclass( definition, sqlalchemy.types.Date ):
-                value = datetime.datetime( year = 2000, month = 1, day = i%31 )                  
-            elif issubclass( definition, sqlalchemy.types.Time ):
-                value = datetime.time( hour = 1, minute = i )
-            elif issubclass( definition, sqlalchemy.types.String ):
-                value = str( i )
-            elif issubclass( definition, sqlalchemy.types.Boolean ):
-                value = True
-            else:
-                value = i
+            value = self.value_for_type( definition, i )
             t = T()
             setattr( t, name, value )
         self.session.flush()
+        self.admin = TAdmin()
         
+    def value_for_type( self, definition, i ):
+        value = i        
+        if issubclass( definition, sqlalchemy.types.DateTime ):
+            value = datetime.datetime( year = 2000, month = 1, day = 1, hour = 1, minute = i )       
+        elif issubclass( definition, sqlalchemy.types.Date ):
+            value = datetime.date( year = 2000, month = 1, day = i%31 )                  
+        elif issubclass( definition, sqlalchemy.types.Time ):
+            value = datetime.time( hour = 1, minute = i )
+        elif issubclass( definition, sqlalchemy.types.String ):
+            value = str( i )
+        elif issubclass( definition, sqlalchemy.types.Boolean ):
+            value = True
+        return value
+            
     def test_search_decorator( self ):
-        pass
+        """Verify it search works for most common types"""
+        from camelot.view.search import create_entity_search_query_decorator
+        for (i,name), definition in types_to_test.items():
+            value = self.value_for_type( definition, i )
+            #
+            # @todo : search for types that need special conversion to string
+            #         is skipped for now because the test would become too
+            #         convoluted, this should work through a to_string field
+            #         attribute.
+            #
+            if isinstance( value, ( datetime.date, datetime.time, bool) ):
+                continue
+            string_value = str( value )
+            
+            search_decorator = create_entity_search_query_decorator( self.admin,
+                                                                     string_value )
+            query = self.session.query( T )
+            query = search_decorator( query )
+            
+            #print query
+            
+            self.assertTrue( query.count() > 0 )
+            
     
