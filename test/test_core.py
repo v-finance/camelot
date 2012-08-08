8265695a5b5e8276e4609a13d322d61ff7b318e8
Erik Janssens <erik.janssens@vortex-financials.be>
2012-08-08 23:32:38 +0200

    complete sql memento implementation

--- test/test_core.py
+++ test/test_core.py
@@ -1,10 +1,46 @@
 import unittest
 from camelot.test import ModelThreadTestCase
 
-class CoreCase(ModelThreadTestCase):
-    """test functions from camelot.core
+memento_id_counter = 0
+
+class MementoCase( ModelThreadTestCase ):
+    """test functions from camelot.core.memento
     """
-    pass
+    
+    def setUp( self ):
+        super( MementoCase, self ).setUp()
+        from camelot.core.memento import SqlMemento
+        global memento_id_counter
+        self.memento = SqlMemento()
+        memento_id_counter += 1
+        self.id_counter = memento_id_counter
+        self.model = 'TestMemento'
+        
+    def test_update( self ):
+        self.memento.register_update( self.model,
+                                      [self.id_counter],
+                                      {'name':'foo'} )
+        changes = list( self.memento.get_changes( self.model,
+                                                  [self.id_counter],
+                                                  {'name':'bar'} ) )
+        self.assertEqual( len(changes), 1 )
+
+    def test_delete( self ):
+        self.memento.register_delete( self.model,
+                                      [self.id_counter],
+                                      {'name':'foo'} )
+        changes = list( self.memento.get_changes( self.model,
+                                                  [self.id_counter],
+                                                  {'name':'foo'} ) )
+        self.assertEqual( len(changes), 1 )
+
+    def test_create( self ):
+        self.memento.register_create( self.model,
+                                      [self.id_counter] )
+        changes = list( self.memento.get_changes( self.model,
+                                                  [self.id_counter],
+                                                  {'name':'foo'} ) )
+        self.assertEqual( len(changes), 1 )
         
 class ConfCase(unittest.TestCase):
     """Test the global configuration"""
