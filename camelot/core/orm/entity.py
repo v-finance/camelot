054a811828ee247f0f0f9fdb097be86e4504fc8e
Erik Janssens <erik.janssens@vortex-financials.be>
2012-09-08 17:20:22 +0200

    split of update_or_create and from_dict to be able to reuse them

--- camelot/core/orm/entity.py
+++ camelot/core/orm/entity.py
@@ -204,7 +204,67 @@ class EntityMeta( DeclarativeMeta ):
 
         if '__table__' in cls.__dict__:
             setattr( cls, 'table', cls.__dict__['__table__'] )
+
+def update_or_create_entity( cls, data, surrogate = True ):
+    mapper = orm.class_mapper( cls )
+    pk_props = mapper.primary_key
+
+    # if all pk are present and not None
+    if not [1 for p in pk_props if data.get( p.key ) is None]:
+        pk_tuple = tuple( [data[prop.key] for prop in pk_props] )
+        record = cls.query.get(pk_tuple)
+        if record is None:
+            record = cls()
+    else:
+        if surrogate:
+            record = cls()
+        else:
+            raise Exception("cannot create non surrogate without pk")
+    dict_to_entity( record, data )
+    return record
         
+def dict_to_entity( entity, data ):
+    """Update a mapped object with data from a JSON-style nested dict/list
+    structure.
+    
+    :param entity: the Entity object into which to store the data
+    :param data: a `dict` with data to store into the entity
+    """
+    # surrogate can be guessed from autoincrement/sequence but I guess
+    # that's not 100% reliable, so we'll need an override
+
+    mapper = orm.object_mapper( entity )
+
+    for key, value in data.iteritems():
+        if isinstance( value, dict ):
+            dbvalue = getattr( entity, key )
+            rel_class = mapper.get_property(key).mapper.class_
+            pk_props = orm.class_mapper( rel_class ).primary_key
+
+            # If the data doesn't contain any pk, and the relationship
+            # already has a value, update that record.
+            if not [1 for p in pk_props if p.key in data] and \
+               dbvalue is not None:
+                dict_to_entity( dbvalue, value )
+            else:
+                record = update_or_create_entity( rel_class, value)
+                setattr(entity, key, record)
+        elif isinstance(value, list) and \
+             value and isinstance(value[0], dict):
+
+            rel_class = mapper.get_property(key).mapper.class_
+            new_attr_value = []
+            for row in value:
+                if not isinstance(row, dict):
+                    raise Exception(
+                            'Cannot send mixed (dict/non dict) data '
+                            'to list relationships in from_dict data.')
+                record = update_or_create_entity( rel_class, row)
+                new_attr_value.append(record)
+            setattr(entity, key, new_attr_value)
+        else:
+            setattr(entity, key, value)
+    
 class EntityBase( object ):
     """A declarative base class that adds some methods that used to be
     available in Elixir"""
@@ -225,66 +285,14 @@ class EntityBase( object ):
 
     @classmethod
     def update_or_create( cls, data, surrogate = True ):
-        
-        mapper = orm.class_mapper( cls )
-        pk_props = mapper.primary_key
-
-        # if all pk are present and not None
-        if not [1 for p in pk_props if data.get( p.key ) is None]:
-            pk_tuple = tuple( [data[prop.key] for prop in pk_props] )
-            record = cls.query.get(pk_tuple)
-            if record is None:
-                if surrogate:
-                    raise Exception("cannot create surrogate with pk")
-                else:
-                    record = cls()
-        else:
-            if surrogate:
-                record = cls()
-            else:
-                raise Exception("cannot create non surrogate without pk")
-        record.from_dict( data )
-        return record
+        return update_or_create_entity( cls, data, surrogate )
     
-    def from_dict(self, data):
+    def from_dict( self, data ):
         """
         Update a mapped class with data from a JSON-style nested dict/list
         structure.
         """
-        # surrogate can be guessed from autoincrement/sequence but I guess
-        # that's not 100% reliable, so we'll need an override
-
-        mapper = orm.object_mapper( self )
-
-        for key, value in data.iteritems():
-            if isinstance(value, dict):
-                dbvalue = getattr(self, key)
-                rel_class = mapper.get_property(key).mapper.class_
-                pk_props = orm.class_mapper( rel_class ).primary_key
-
-                # If the data doesn't contain any pk, and the relationship
-                # already has a value, update that record.
-                if not [1 for p in pk_props if p.key in data] and \
-                   dbvalue is not None:
-                    dbvalue.from_dict(value)
-                else:
-                    record = rel_class.update_or_create(value)
-                    setattr(self, key, record)
-            elif isinstance(value, list) and \
-                 value and isinstance(value[0], dict):
-
-                rel_class = mapper.get_property(key).mapper.class_
-                new_attr_value = []
-                for row in value:
-                    if not isinstance(row, dict):
-                        raise Exception(
-                                'Cannot send mixed (dict/non dict) data '
-                                'to list relationships in from_dict data.')
-                    record = rel_class.update_or_create(row)
-                    new_attr_value.append(record)
-                setattr(self, key, new_attr_value)
-            else:
-                setattr(self, key, value)
+        return dict_to_entity( self, data )
 
     def to_dict( self, deep = {}, exclude = [] ):
         """Generate a JSON-style nested dict/list structure from an object."""
