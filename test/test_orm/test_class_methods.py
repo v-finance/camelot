530ea866bc46a7cd3d5c513c153e814aaa1eb2e0
Erik Janssens <erik.janssens@vortex-financials.be>
2012-10-28 12:58:11 +0000

    use hybrid extension for query class attribute

--- test/test_orm/test_class_methods.py
+++ test/test_orm/test_class_methods.py
@@ -2,13 +2,15 @@
     simple test case
 """
 
+import threading
+
 from . import TestMetaData
 
-from camelot.core.orm import Field
+from camelot.core.orm import Field, Session
 
 from sqlalchemy.types import String, Unicode, Integer
 
-class TestOldMethods( TestMetaData ):
+class TestClassMethods( TestMetaData ):
 
     def test_get( self ):
         
@@ -21,6 +23,36 @@ class TestOldMethods( TestMetaData ):
             a1 = A(name="a1")
 
         self.session.expire_all()
-
-        assert A.get(1).name == "a1"
-
+        self.assertEqual(  A.get(1).name, "a1" )
+        
+    def test_query( self ):
+        #
+        # The query attribute of a class should return a query bound to
+        # the session belonging to the current thread
+        #
+        
+        class A( self.Entity ):
+            name = Field(String(32))
+            
+        self.create_all()
+        
+        with self.session.begin():
+            a1 = A(name="a1")
+            
+        self.assertEqual( A.query.count(), 1 )
+        
+        session_1 = A.query.session
+        self.assertEqual( session_1, Session() )
+        
+        class QueryThread( threading.Thread ):
+            
+            def run( self ):
+                self.query_session_2 = A.query.session
+                self.orm_session_2 = Session()
+        
+        thread = QueryThread()
+        thread.start()
+        thread.join()
+        
+        self.assertNotEqual( session_1, thread.orm_session_2 )
+        self.assertNotEqual( session_1, thread.query_session_2 )
