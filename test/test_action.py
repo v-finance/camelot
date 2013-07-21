f6e78455ac531a4ac1e4f8fc9ceba01ad85e3c4f
Erik Janssens <erik.janssens@vortex-financials.be>
2013-07-21 12:50:53 +0200

    unittest for the authentication action

--- test/test_action.py
+++ test/test_action.py
@@ -24,6 +24,8 @@ from test_view import static_images_path
 import test_proxy
 import test_model
 
+test_images = [os.path.join( os.path.dirname(__file__), '..', 'camelot_example', 'media', 'covers', 'circus.png') ]
+
 class ActionBaseCase( ModelThreadTestCase ):
 
     def setUp(self):
@@ -125,7 +127,7 @@ class ActionStepsCase( ModelThreadTestCase ):
         select_file = generator.next()
         self.assertFalse( select_file.single )
         # pretend the user selected a file
-        generator.send( [os.path.join( os.path.dirname(__file__), '..', 'camelot_example', 'media', 'covers', 'circus.png') ] )
+        generator.send(test_images)
         # continue the action till the end
         list( generator )
         # a movie should be inserted
@@ -628,12 +630,20 @@ class ApplicationActionsCase( test_model.ExampleModelCase ):
         from camelot.core.files.storage import Storage
         from camelot.view.workspace import DesktopWorkspace
         self.app_admin = ApplicationAdmin()
-        self.context = MockModelContext()
+        self.context = MockModelContext(session=self.session)
         self.storage = Storage()
         self.gui_context = application_action.ApplicationActionGuiContext()
         self.gui_context.admin = self.app_admin
         self.gui_context.workspace = DesktopWorkspace( self.app_admin, None )
 
+    def test_authentication(self):
+        action = application_action.Authentication()
+        self.assertTrue( action.get_state(self.context) )
+        generator = action.model_run(self.context)
+        for step in generator:
+            if isinstance(step, action_steps.SelectFile):
+                generator.send(test_images)
+        
     def test_refresh( self ):
         from camelot.core.orm import Session
         from camelot.model.party import Person
