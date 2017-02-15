ae8ea1fbd9a80edffc3db99ee83f71d190d6f9bc
Pieter-Jan Delaruelle <pieter-jan.delaruelle@vortex-financials.be>
2017-02-15 09:07:50 +0100

    Make it work, even when the field_attribute is not set

--- camelot/view/controls/editors/floateditor.py
+++ camelot/view/controls/editors/floateditor.py
@@ -192,7 +192,7 @@ class FloatEditor(CustomEditor):
         editable = kwargs.get('editable', False)
         self.calculatorButton.setShown(editable and self._calculator)
         spinBox = self.findChild(CustomDoubleSpinBox, 'spinbox')
-        spinBox.setFocusPolicy(kwargs.get('focus_policy'))
+        spinBox.setFocusPolicy(kwargs.get('focus_policy', Qt.StrongFocus))
         spinBox.setToolTip(six.text_type(kwargs.get('tooltip') or ''))
         spinBox.setPrefix(six.text_type(kwargs.get('prefix', '')))
         spinBox.setSuffix(six.text_type(kwargs.get('suffix', '')))
