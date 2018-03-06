4a9656cf75b85b9974d8fb7994bd4bc389bedc69
Jeff Horemans <jeff.horemans@vortex-financials.be>
2018-03-06 17:58:03 +0100

    Changed foreign key naming convention to generate shorter key names.

--- camelot/core/sql.py
+++ camelot/core/sql.py
@@ -48,7 +48,7 @@ convention={
     "ix": "ix_%(column_0_label)s",
     "uq": "uq_%(table_name)s_%(column_0_name)s",
     "ck": "ck_%(table_name)s_%(constraint_name)s",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s",
     "pk": "pk_%(table_name)s"
 }
 metadata = MetaData(naming_convention=convention)
