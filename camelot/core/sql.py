f7d506bc9496c26942fe144534f8b852837d7da9
Jeff Horemans <jeff.horemans@vortex-financials.be>
2018-03-06 10:28:48 +0100

    Added SQLAlchemy constraint naming convention

--- camelot/core/sql.py
+++ camelot/core/sql.py
@@ -44,7 +44,14 @@ LOGGER = logging.getLogger('camelot.core.sql')
 # Singleton metadata object, to be used in SQLAlchemy
 # setups with only a single database
 #
-metadata = MetaData()
+convention={
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+metadata = MetaData(naming_convention=convention)
 metadata.autoflush = False
 metadata.transactional = False
 
