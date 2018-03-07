b710b085ecb292f0c35613926fb739b536aa133a
Jeff Horemans <jeff.horemans@vortex-financials.be>
2018-03-07 09:29:40 +0100

    Renamed check constraint naming convention

--- camelot/core/sql.py
+++ camelot/core/sql.py
@@ -47,7 +47,7 @@ LOGGER = logging.getLogger('camelot.core.sql')
 convention={
     "ix": "ix_%(column_0_label)s",
     "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
     "fk": "fk_%(table_name)s_%(column_0_name)s",
     "pk": "pk_%(table_name)s"
 }
