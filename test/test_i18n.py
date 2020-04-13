448f32b5e286136d114baeac7fd785425fc74230
Erik Janssens <erik.janssens@vortex-financials.be>
2020-04-13 21:01:06 +0200

    qt auto sets country when only specifying language

--- test/test_i18n.py
+++ test/test_i18n.py
@@ -1,3 +1,4 @@
+from camelot.admin.application_admin import ApplicationAdmin
 from camelot.core.qt import QtCore
 from camelot.test import ModelThreadTestCase
 
@@ -12,12 +13,8 @@ class I18NTest(ModelThreadTestCase):
         self.assertTrue( message )
         
     def test_load_translators( self ):
-        from camelot.admin.application_admin import ApplicationAdmin
         app_admin = ApplicationAdmin()
-        QtCore.QLocale.setDefault( QtCore.QLocale( 'nl_BE' ) )
-        self.assertEqual( len( app_admin.get_translator() ), 1 )
-        QtCore.QLocale.setDefault( QtCore.QLocale( 'pt' ) )
-        self.assertEqual( len( app_admin.get_translator() ), 1 )
-
-        QtCore.QLocale.setDefault( QtCore.QLocale( 'pt_BR' ) )
-        self.assertEqual( len( app_admin.get_translator() ), 2 )
+        QtCore.QLocale.setDefault(QtCore.QLocale('nl_BE'))
+        self.assertEqual(len( app_admin.get_translator() ), 1)
+        QtCore.QLocale.setDefault(QtCore.QLocale('pt_BR'))
+        self.assertEqual(len( app_admin.get_translator() ), 2 )
