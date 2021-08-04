a6097893abd2e5e5db7aad824cd9a81e6fe9f53a
Student 2 <student.2@vortex-financials.be>
2021-08-04 16:06:29 +0200

    VFIN-1563 #comment Refactored Mode to use post_init

--- camelot/admin/action/base.py
+++ camelot/admin/action/base.py
@@ -27,7 +27,7 @@
 #
 #  ============================================================================
 
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, InitVar
 from enum import Enum
 import logging
 import typing
@@ -151,21 +151,12 @@ the default mode.
     """
 
     name: str
-    verbose_name: typing.Union[str, ugettext_lazy]
-    icon: typing.Union[Icon, None]
+    verbose_name: typing.Union[str, ugettext_lazy] = None
+    icon: typing.Union[Icon, None] = None
     
-    def __init__( self, name, verbose_name=None, icon=None):
-        """
-        :param name: the name of the mode, as it will be passed to the
-            gui_run and model_run method
-        :param verbose_name: the name shown to the user
-        :param icon: the icon of the mode
-        """
-        self.name = name
-        if verbose_name is None:
-            verbose_name = name.capitalize()
-        self.verbose_name = verbose_name
-        self.icon = icon
+    def __post_init__(self):
+        if self.verbose_name is None:
+            self.verbose_name = self.name.capitalize()
 
     def render( self, parent ):
         """Create a :class:`QtWidgets.QAction` that can be used to enable widget
