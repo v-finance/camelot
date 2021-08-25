234bda34f71fc075942f9a493723e7c2a734b608
Student1 vFinance <student1@vortex-financials.be>
2021-08-25 10:44:21 +0200

    VFIN-1571 #comment implemented get_completions logic to DataclassAdmin

--- camelot/admin/dataclass_admin.py
+++ camelot/admin/dataclass_admin.py
@@ -2,6 +2,7 @@ import dataclasses
 import typing
 
 from camelot.admin.object_admin import ObjectAdmin
+from camelot.admin.action import list_filter
 from camelot.view.field_attributes import _dataclass_to_python_type
 from camelot.view.controls import delegates
 from camelot.core.orm import Entity
@@ -42,6 +43,28 @@ class DataclassAdmin(ObjectAdmin):
                field_type.__origin__ == typing.Union and \
                len(field_type.__args__) == 2 and \
                type(None) == field_type.__args__[1]
+    
+    def get_completions(self, obj, field_name, prefix):
+        """
+        Overwrites `ObjectAdmin.get_completions` and searches for autocompletion
+        along relationships.
+        """
+        for field in dataclasses.fields(self.entity):
+            if field.name == field_name and issubclass(field.type, Entity):
+                all_attributes = self.get_field_attributes(field_name)
+                admin = all_attributes.get('admin')
+                session = self.get_session(obj)
+                if (admin is not None) and (session is not None):
+                    search_filter = list_filter.SearchFilter(admin)
+                    query = admin.get_query(session)
+                    query = search_filter.decorate_query(query, prefix)
+                    return [e for e in query.limit(20).all()]
+                return super().get_completions(obj, field_name, prefix)
+            
+    def get_session(self, obj):
+        raise NotImplementedError
+    
+    
     
               
         
