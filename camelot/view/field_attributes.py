27155a135fa78ffd769dd1edeb54fcfdee40ee91
Jeff Horemans <jeff.horemans@vortex-financials.be>
2021-11-04 11:44:30 +0100

    VFIN-1655 #comment Set default filter_strategy field_attribute for Enumeration column type to ChoicesFilter.

--- camelot/view/field_attributes.py
+++ camelot/view/field_attributes.py
@@ -190,7 +190,7 @@ _sqlalchemy_to_python_type_ = {
         'widget': 'combobox',
         'to_string': enumeration_to_string,
         'search_strategy': list_filter.NoFilter,
-        'filter_strategy': list_filter.NoFilter,
+        'filter_strategy': list_filter.ChoicesFilter,
     },
 
     camelot.types.Language: lambda f: {
