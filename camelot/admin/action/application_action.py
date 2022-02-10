f3d783ca4391e5f8ff00cba5ba5fe54caa171ffb
Tim Vandermeersch <tim.vandermeersch@vortex-financials.be>
2022-02-11 00:04:26 +0100

    VFIN-1622 #comment Use C++ UpdateProgress

--- camelot/admin/action/application_action.py
+++ camelot/admin/action/application_action.py
@@ -108,10 +108,10 @@ class ApplicationActionGuiContext( GuiContext ):
         self.action_routes = {}
     
     def get_progress_dialog(self):
-        from camelot.view.qml_view import get_qml_window
-        window = get_qml_window()
-        if window is not None and window.isVisible():
-            progress_dialog = window.findChild(QtCore.QObject, 'qml_progress_dialog')
+        from camelot.view.qml_view import get_qml_root_backend
+        root_backend = get_qml_root_backend()
+        if root_backend is not None and root_backend.isVisible():
+            progress_dialog = root_backend.progressDialog()
             if progress_dialog is not None:
                 return progress_dialog
 
