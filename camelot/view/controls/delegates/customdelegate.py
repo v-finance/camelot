322197f049b3e87cc500b1694c8fe5224dfd9af6
Jeff Horemans <jeff.horemans@vortex-financials.be>
2022-04-13 13:12:27 +0200

    VFIN-1869 #comment Convert choices' values to bindings in CustomDelegate's get_standard_item, and data set them using the newly introduced ChoicesRole.

--- camelot/view/controls/delegates/customdelegate.py
+++ camelot/view/controls/delegates/customdelegate.py
@@ -32,6 +32,8 @@ import json
 import logging
 import dataclasses
 
+from camelot.core.naming import initial_naming_context
+
 from ....core.qt import (QtGui, QtCore, QtWidgets, Qt,
                          py_to_variant, variant_to_py)
 from ....core.serializable import json_encoder
@@ -174,6 +176,10 @@ class CustomDelegate(QtWidgets.QItemDelegate):
                      Qt.ItemDataRole.ToolTipRole)
         item.setData(py_to_variant(model_context.field_attributes.get('background_color')),
                      Qt.ItemDataRole.BackgroundRole)
+        choices = model_context.field_attributes.get('choices')
+        if choices is not None:
+            choices = [(initial_naming_context._bind_object(obj), verbose_name) for obj, verbose_name in choices]
+        item.setData(py_to_variant(choices), Qt.ItemDataRole.ChoicesRole)
         return item
 
     def createEditor(self, parent, option, index):
