b3a6e365f3074d97ba85feed3234c6cb2d4fa085
Jeff Horemans <jeff.horemans@vortex-financials.be>
2022-04-13 15:01:39 +0200

    VFIN-1865 #comment Move scrolling to last row from AddNewObjectMixin to AddNewObject list action.

--- camelot/admin/action/list_action.py
+++ camelot/admin/action/list_action.py
@@ -1161,8 +1161,6 @@ class AddNewObjectMixin(object):
         )
         if create_inline is False:
             yield action_steps.OpenFormView(new_object, admin.get_proxy([new_object]), admin)
-            # Scroll to last row so that the user sees the newly added object in the list.
-            yield action_steps.ToLastRow()
 
 class AddNewObject( AddNewObjectMixin, EditAction ):
     """Add a new object to a collection. Depending on the
@@ -1187,6 +1185,12 @@ class AddNewObject( AddNewObjectMixin, EditAction ):
 
     def get_proxy(self, model_context, admin):
         return model_context.proxy
+
+    def model_run(self, model_context, mode):
+        from camelot.view import action_steps
+        yield from super().model_run(model_context, mode)
+        # Scroll to last row so that the user sees the newly added object in the list.
+        yield action_steps.ToLastRow()
 
 add_new_object = AddNewObject()
 
