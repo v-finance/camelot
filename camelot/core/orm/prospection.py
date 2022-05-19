e02169afa5f0706034e968ef2f51f16c86754d2f
Jeff Horemans <jeff.horemans@vortex-financials.be>
2022-05-19 13:50:32 +0200

    VFIN-1877 #comment Use new application_date entity arg to check prospective_attribute's prospection applicability.

--- camelot/core/orm/prospection.py
+++ camelot/core/orm/prospection.py
@@ -1,5 +1,6 @@
 import functools
 
+from camelot.model.authentication import end_of_times
 from sqlalchemy import orm, sql
 
 def prospective_attribute(attribute):
@@ -14,8 +15,9 @@ def prospective_attribute(attribute):
         if prospection on the instance is not applicable.
         A default _prospection_applicable method is provided on each Entity class by `orm.entity.EntityMeta` metaclass,
         but can be customized if needed on the corresponding entity.
-    """
 
+    This function decorator requires the application_date of the target Entity class to be registered in its __entity_args__.
+    """
     assert isinstance(attribute, (sql.schema.Column, orm.attributes.InstrumentedAttribute))
     class decorator:
 
@@ -26,12 +28,19 @@ def prospective_attribute(attribute):
         def __get__(self, instance, owner):
             mapper = orm.class_mapper(owner)
             class_attribute = mapper.get_property(attribute.key).class_attribute
+            assert owner._get_entity_arg('application_date') is not None, \
+                   "Defining prospective attributes requires the application_date "\
+                   "to be registered in the Entity's __entity_args__."
+            application_date_prop = mapper.get_property(owner._get_entity_arg('application_date').key)
 
             @functools.wraps(self.func)
             def wrapper(target, at):
+                application_date = application_date_prop.class_attribute.__get__(target, None)
                 if None not in (target, at):
                     current_value = class_attribute.__get__(target, None)
-                    if self.owner._prospection_applicable(target, at):
+                    # The prospection should only be possible if the target's is applicable.
+                    # Otherwise, the current value is returned.
+                    if not (application_date >= end_of_times() or at < application_date):
                         return self.func(target, at)
                     return current_value
             return wrapper
