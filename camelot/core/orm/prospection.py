872433e806e31317c6ebd72e4c7d245bb8eaf5a4
Jeff Horemans <jeff.horemans@vortex-financials.be>
2022-05-25 10:43:37 +0200

    VFIN-1877 #comment Removed target instance applicability check from prospected attribute decorator.

--- camelot/core/orm/prospection.py
+++ camelot/core/orm/prospection.py
@@ -34,20 +34,13 @@ class abstract_attribute_prospection(object):
 
     def __call__(self, target, at, **kwargs):
         target_cls = type(target)
-        mapper = orm.class_mapper(target_cls)
-        class_attribute = mapper.get_property(self.__class__.attribute.key).class_attribute
         if self.for_transition_types:
             assert target_cls.transition_types is not None, '{} has no transition_types configured in its __entity_args__'.format(target_cls)
             for transition_type in self.for_transition_types:
                 assert transition_type in target_cls.transition_types.values(), '{} is not a valid transition type for {}'.format(transition_type, target_cls)
 
         if None not in (target, at):
-            current_value = class_attribute.__get__(target, None)
-            # The prospection should only be possible if the target's is applicable.
-            if target.is_applicable_at(at):
-                return self.func(target, at, **kwargs)
-            # Otherwise, the current value is returned.
-            return current_value
+            return self.func(target, at, **kwargs)
 
     def __get__(self, instance, owner):
         return types.MethodType(self, instance) if instance is not None else self
@@ -57,15 +50,7 @@ def prospected_attribute(column_attribute, *transition_types):
     Function decorator that supports registering prospected behaviour for one of the instrumented
     column attribute of an Entity class.
     The user-defined function expects an instance of the target entity and the prospection date,
-    and will be decorated/wrapped with the following behaviour:
-
-      * the return value will be undefined if any of the arguments are undefined
-      * the return value will be the current value of the given column attribute on the target instance,
-        if prospection on the instance is not applicable.
-        A default _prospection_applicable method is provided on each Entity class by `orm.entity.EntityMeta` metaclass,
-        but can be customized if needed on the corresponding entity.
-
-    This function decorator requires the application_date of the target Entity class to be registered in its __entity_args__.
+    and will be decorated/wrapped to be undefined if the provided target instance or prospection date are undefined.
 
     :example:
      |
@@ -74,10 +59,6 @@ def prospected_attribute(column_attribute, *transition_types):
      |     apply_from_date = schema.Column(sqlalchemy.types.Date())
      |     duration = schema.Column(sqlalchemy.types.Integer())
      |
-     |     __entity_args__ = {
-     |        'application_date': apply_from_date,
-     |     }
-     |
      |     @prospected_attribute(duration)
      |     def prospected_duration(self, at):
      |        return months_between_dates(self.apply_from_date, at)
@@ -85,8 +66,6 @@ def prospected_attribute(column_attribute, *transition_types):
      |  ConcreteEntity(apply_from_date=datetime.date(2012,1,1), duration=24).prospected_duration(datetime.date(2013,1,1)) == 12
      |  ConcreteEntity(apply_from_date=datetime.date(2012,1,1), duration=24).prospected_duration(None) == None
      |  ConcreteEntity(apply_from_date=None, duration=24).prospected_duration(None) == 24
-     |  ConcreteEntity(apply_from_date=datetime.date(2012,1,1), duration=24).prospected_duration(datetime.date(2011,1,1)) == 24
-     |  ConcreteEntity(apply_from_date=datetime.date(2401,1,1), duration=24).prospected_duration(datetime.date(2012,1,1)) == 24
     """
     assert is_supported_attribute(column_attribute), 'The given attribute should be a valid column attribute'
 
