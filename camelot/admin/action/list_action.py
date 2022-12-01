b456d7b970c91fa68b9707cf6689ab8307394d70
Jeff Horemans <jeff.horemans@vortex-financials.be>
2022-12-01 11:57:12 +0100

    VFIN-2157 #comment Added some commentary on AddNewObjectMixin's admin resolving after object creation.

--- camelot/admin/action/list_action.py
+++ camelot/admin/action/list_action.py
@@ -957,7 +957,9 @@ class AddNewObjectMixin(object):
             raise RuntimeError("Action's model_run() called on noneditable entity")
         create_inline = model_context.field_attributes.get('create_inline', False)
         new_object = yield from self.create_object(model_context, admin, mode)
-        # Deduct admin again as it may have gotten secondary discriminators set.
+        # Resolve admin again after the new object has been created, as it may
+        # have gotten secondary discriminators set.
+        # So only at this point we can be certain of the discriminatory value.
         admin = self.get_admin(model_context, admin.get_discriminator_value(new_object))
         subsystem_object = admin.get_subsystem_object(new_object)
         # if the object is valid, flush it, but in ancy case inform the gui
