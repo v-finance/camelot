ca0cca8fa531550ab4e62b3844d995971530d1bb
Tim Vandermeersch <tim.vandermeersch@vortex-financials.be>
2022-12-14 15:39:51 +0100

    VFIN-2183 #comment Fix one2many field actions

--- camelot/view/controls/editors/one2manyeditor.py
+++ camelot/view/controls/editors/one2manyeditor.py
@@ -32,7 +32,8 @@ import logging
 
 from camelot.view.proxy.collection_proxy import CollectionProxy
 from ....admin.action.base import GuiContext
-from ....core.qt import Qt, QtCore, QtWidgets, variant_to_py
+from ....core.qt import Qt, QtCore, QtWidgets, variant_to_py, is_deleted
+from ....core.item_model import ActionModeRole
 from ... import gui_naming_context
 from ..view import ViewWithActionsMixin
 from ..tableview import TableWidget
@@ -189,13 +190,25 @@ class One2ManyEditor(CustomEditor, WideEditor, ViewWithActionsMixin, GuiContext)
         if table is not None:
             return table.model()
 
+    @QtCore.qt_slot(list, object)
+    def editorActionTriggered(self, route, mode):
+        table = self.findChild(QtWidgets.QWidget, 'table')
+        if table is not None:
+            model = table.model()
+            if model is None:
+                return
+            if is_deleted(model):
+                return
+            index = table.currentIndex()
+            model.setData(index, json.dumps([route, mode]), ActionModeRole)
+
     @QtCore.qt_slot(object)
     def set_columns(self, columns):
         from ..delegates.delegatemanager import DelegateManager
         table = self.findChild(QtWidgets.QWidget, 'table')
         if table is not None:
             delegate = DelegateManager(parent=self)
-            delegate.actionTriggered.connect(self.actionTriggered)
+            delegate.actionTriggered.connect(self.editorActionTriggered)
             table.setItemDelegate(delegate)
             model = table.model()
             if model is not None:
