ae2de9befb4dae9057d14c576c7907a09176d5d1
Erik Janssens <erik.janssens@vortex-financials.be>
2023-05-25 17:19:24 +0200

    VFIN-1890 #comment run all tests in separate process

--- camelot/test/__init__.py
+++ camelot/test/__init__.py
@@ -46,7 +46,6 @@ from ..core.qt import Qt, QtCore, QtGui, QtWidgets
 from ..view.action_runner import action_runner, GuiRun
 from ..view.model_process import ModelProcess
 from ..view import model_thread, action_steps
-from ..view.model_thread.signal_slot_model_thread import SignalSlotModelThread
 
 has_programming_error = False
 
@@ -168,27 +167,6 @@ class ActionMixinCase(object):
         action_runner.wait_for_completion()
         return gui_run.steps
 
-
-class RunningThreadCase(unittest.TestCase, ActionMixinCase):
-    """
-    Test case that starts a model thread when setting up the case class
-    """
-
-    @classmethod
-    def setUpClass(cls):
-        cls.thread = SignalSlotModelThread()
-        model_thread._model_thread_.insert(0, cls.thread)
-        cls.thread.start()
-
-    @classmethod
-    def tearDownClass(cls):
-        model_thread._model_thread_.remove(cls.thread)
-        cls.thread.stop()
-
-    @classmethod
-    def process(cls):
-        """Wait until all events are processed and the queues of the model thread are empty"""
-        action_runner.wait_for_completion()
 
 class RunningProcessCase(unittest.TestCase, ActionMixinCase):
     """
