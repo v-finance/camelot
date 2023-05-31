fd8175f89226f69bade4aeeb7765a31e83048d03
Tim Vandermeersch <tim.vandermeersch@vortex-financials.be>
2023-05-31 14:52:10 +0200

    VFIN-2254 #comment Anonimize first/last name in archive

--- camelot/sql/types.py
+++ camelot/sql/types.py
@@ -2,35 +2,40 @@ from sqlalchemy.types import Unicode, Date, Numeric
 
 from camelot.data import types
 
+
+def first_letter_transform(data):
+    return data[0] if data else data
+
+
 class SensitiveType:
 
-    def __init__(self, sensitivity_level):
+    def __init__(self, sensitivity_level, transform=None):
         self.sensitivity_level = sensitivity_level
+        self.transform = transform
 
 class SensitiveUnicode(SensitiveType, Unicode):
     """
     Custom SQLAlchemy Unicode type for sensitive data.
     """
 
-    def __init__(self, sensitivity_level, *args, **kwargs):
-        SensitiveType.__init__(self, sensitivity_level)
+    def __init__(self, sensitivity_level, transform=None, *args, **kwargs):
+        SensitiveType.__init__(self, sensitivity_level, transform)
         Unicode.__init__(self, *args, **kwargs)
 
-def IdentifyingUnicode(*args, **kwargs):
-    return SensitiveUnicode(types.sensitivity_levels.identifying, *args, **kwargs)
+def IdentifyingUnicode(transform=None, *args, **kwargs):
+    return SensitiveUnicode(types.sensitivity_levels.identifying, transform, *args, **kwargs)
 
-def QuasiIdentifyingUnicode(*args, **kwargs):
-    return SensitiveUnicode(types.sensitivity_levels.quasi_identifying, *args, **kwargs)
+def QuasiIdentifyingUnicode(transform=None, *args, **kwargs):
+    return SensitiveUnicode(types.sensitivity_levels.quasi_identifying, transform, *args, **kwargs)
 
-def SensitivePersonalUnicode(*args, **kwargs):
-    return SensitiveUnicode(types.sensitivity_levels.sensitive_peronsal, *args, **kwargs)
+def SensitivePersonalUnicode(transform=None, *args, **kwargs):
+    return SensitiveUnicode(types.sensitivity_levels.sensitive_peronsal, transform, *args, **kwargs)
 
-def SensitiveFinancialUnicode(*args, **kwargs):
-    return SensitiveUnicode(types.sensitivity_levels.sensitive_financial, *args, **kwargs)
+def SensitiveFinancialUnicode(transform=None, *args, **kwargs):
+    return SensitiveUnicode(types.sensitivity_levels.sensitive_financial, transform, *args, **kwargs)
 
-def SensitiveHealthUnicode(*args, **kwargs):
-    return SensitiveUnicode(types.sensitivity_levels.sensitive_health, *args, **kwargs)
-
+def SensitiveHealthUnicode(transform=None, *args, **kwargs):
+    return SensitiveUnicode(types.sensitivity_levels.sensitive_health, transform, *args, **kwargs)
 
 
 class SensitiveDate(SensitiveType, Date):
@@ -38,8 +43,8 @@ class SensitiveDate(SensitiveType, Date):
     Custom SQLAlchemy Date type for sensitive data.
     """
 
-    def __init__(self, sensitivity_level, *args, **kwargs):
-        SensitiveType.__init__(self, sensitivity_level)
+    def __init__(self, sensitivity_level, transform=None, *args, **kwargs):
+        SensitiveType.__init__(self, sensitivity_level, transform)
         Date.__init__(self, *args, **kwargs)
 
 def IdentifyingDate(*args, **kwargs):
@@ -63,8 +68,8 @@ class SensitiveNumeric(SensitiveType, Numeric):
     Custom SQLAlchemy Numeric type for sensitive data.
     """
 
-    def __init__(self, sensitivity_level, *args, **kwargs):
-        SensitiveType.__init__(self, sensitivity_level)
+    def __init__(self, sensitivity_level, transform=None, *args, **kwargs):
+        SensitiveType.__init__(self, sensitivity_level, transform)
         Numeric.__init__(self, *args, **kwargs)
 
 def IdentifyingNumeric(*args, **kwargs):
