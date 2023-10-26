8626d1d2ab0e53111430f48c452de66a3ed25c31
Jeff Horemans <jeff.horemans@vortex-financials.be>
2023-10-26 18:10:56 +0200

    VFIN-2001 #comment Fix typo in Camelot sensitive sql types.

--- camelot/sql/types.py
+++ camelot/sql/types.py
@@ -56,7 +56,7 @@ def QuasiIdentifyingDate(*args, transform=None, **kwargs):
     return SensitiveDate(types.sensitivity_levels.quasi_identifying, *args, transform=transform, **kwargs)
 
 def SensitivePersonalDate(*args, transform=None, **kwargs):
-    return SensitiveDate(types.sensitivity_levels.sensitive_peronsal, *args, transform=transform, **kwargs)
+    return SensitiveDate(types.sensitivity_levels.sensitive_personal, *args, transform=transform, **kwargs)
 
 def SensitiveFinancialDate(*args, transform=None, **kwargs):
     return SensitiveDate(types.sensitivity_levels.sensitive_financial, *args, transform=transform, **kwargs)
@@ -81,7 +81,7 @@ def QuasiIdentifyingNumeric(*args, transform=None, **kwargs):
     return SensitiveNumeric(types.sensitivity_levels.quasi_identifying, *args, transform=transform, **kwargs)
 
 def SensitivePersonalNumeric(*args, transform=None, **kwargs):
-    return SensitiveNumeric(types.sensitivity_levels.sensitive_peronsal, *args, transform=transform, **kwargs)
+    return SensitiveNumeric(types.sensitivity_levels.sensitive_personal, *args, transform=transform, **kwargs)
 
 def SensitiveFinancialNumeric(*args, transform=None, **kwargs):
     return SensitiveNumeric(types.sensitivity_levels.sensitive_financial, *args, transform=transform, **kwargs)
