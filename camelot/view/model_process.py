4e0b668737be77d70b163e350c810976244e9f21
Tim Vandermeersch <tim.vandermeersch@vortex-financials.be>
2024-05-06 15:36:04 +0200

    Revert "VFIN-2787 #comment Fix cancel (WIP)"
    This reverts commit 31ead0df198b708a495fe4891c8fa40db7ec03b8.

--- camelot/view/model_process.py
+++ camelot/view/model_process.py
@@ -1,14 +1,12 @@
-import json
 import logging
 from multiprocessing import connection
 import multiprocessing as _mp
-import collections
 
 from ..core.backend import get_root_backend
 from ..core.qt import QtCore
 from ..core.serializable import DataclassSerializable
 from .responses import ActionStepped, Busy
-from .requests import StopProcess, AbstractRequest, CancelAction
+from .requests import StopProcess, AbstractRequest
 
 LOGGER = logging.getLogger(__name__)
 
@@ -30,7 +28,6 @@ class PipeResponseHandler(object):
             response.step = (response.step[0], response.step[1]._to_dict())
         self._response_connection.send_bytes(response._to_bytes())
 
-    # FIXME: remove this
     def has_cancel_request(self):
         return False
 
@@ -39,8 +36,7 @@ class ModelProcess(spawned_mp.Process):
 
     def __init__(self):
         super().__init__()
-        self._process_queue = spawned_mp.JoinableQueue()
-        self._request_queue = collections.deque()
+        self._request_queue = spawned_mp.JoinableQueue()
         self.listener = connection.Listener(('127.0.0.1', 0), 'AF_INET')
         self._address = self.listener.address
 
@@ -80,32 +76,6 @@ class ModelProcess(spawned_mp.Process):
         """
         pass
 
-    def transfer_queue(self, request=None):
-
-        def append_request(req):
-            self._process_queue.task_done()
-            self._request_queue.append(json.loads(req))
-
-        if request is not None:
-            append_request(request)
-        while not self._process_queue.empty():
-            req = self._process_queue.get()
-            append_request(req)
-
-    def has_cancel_request(self):
-
-        def is_cancel_request(request):
-            return request[0] == 'CancelAction'
-
-        self.transfer_queue()
-        for request in self._request_queue:
-            if is_cancel_request(request):
-                LOGGER.info('Found CancelAction in request queue')
-                # remove CancelAction request from queue
-                self._request_queue = collections.deque([r for r in self._request_queue if not is_cancel_request(r)])
-                return True
-        return False
-
     def run(self):
         LOGGER = logging.getLogger("model_process")
         self.initialize()
@@ -115,13 +85,11 @@ class ModelProcess(spawned_mp.Process):
         response_handler = PipeResponseHandler(self._response_sender)
         while True:
             response_handler.send_response(Busy(False))
-            request = self._process_queue.get()
-            self.transfer_queue(request)
-            request = self._request_queue.popleft()
+            request = self._request_queue.get()
             response_handler.send_response(Busy(True))
             try:
                 AbstractRequest.handle_request(
-                    request, response_handler, self
+                    request, response_handler, response_handler
                 )
             except Exception as e:
                 LOGGER.error('Unhandled exception in model process', exc_info=e)
@@ -132,13 +100,15 @@ class ModelProcess(spawned_mp.Process):
                 raise
             except:
                 LOGGER.error('Unhandled event in model process')
+            finally:
+                self._request_queue.task_done()
         LOGGER.info("Terminated")
 
     def post(self, request):
-        if self._process_queue is None:
+        if self._request_queue is None:
             LOGGER.error('Request posted to no longer running process {}'.format(request.data()))
             raise Exception('Process no longer running')
-        self._process_queue.put(request.data())
+        self._request_queue.put(request.data())
         return ['process', str(self.pid)]
 
     def stop(self):
@@ -148,9 +118,9 @@ class ModelProcess(spawned_mp.Process):
         get_root_backend().action_runner().request.disconnect(self.post)
         # make sure no messages can be send to the request queue, after
         # the stop_request was send
-        process_queue = self._process_queue
-        self._process_queue = None
-        process_queue.put(stop_request._to_bytes())
+        request_queue = self._request_queue
+        self._request_queue = None
+        request_queue.put(stop_request._to_bytes())
         self.join()
         # as per Qt documentation, explicit disabling of the notifier is advised
         self.socket_notifier.setEnabled(False)
