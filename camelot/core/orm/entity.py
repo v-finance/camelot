2b8d2276029c7427f7be787ea32284a9550b86cc
Jeff Horemans <jeff.horemans@vortex-financials.be>
2025-07-04 13:45:47 +0200

    VFIN-3558 #comment Add current status property to EntityBase.

--- camelot/core/orm/entity.py
+++ camelot/core/orm/entity.py
@@ -379,3 +379,12 @@ class EntityBase( object ):
         assert isinstance(at, datetime.date)
         application_date = entity.endpoint.application_date.__get__(self, None)
         return application_date is not None and not (application_date >= end_of_times or at < application_date)
+
+    @property
+    def current_status(self):
+        """
+        Return the current status of this entity instance.
+        By default, the status is determined by the `__status__` attribute of the entity class.
+        Override this method in subclasses to provide custom status handling on the instance level.
+        """
+        return self.__status__
